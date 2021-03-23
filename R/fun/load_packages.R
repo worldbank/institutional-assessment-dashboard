a537f87b506e3cdfeefb6dfc14482ac223dd16be
gabrielvazdemelo <gabrielvazdemelo@gmail.com>
2021-03-23 00:27:35 -0300

    Add load_packages.R

---
+++ R/fun/load_packages.R
@@ -0,0 +1,9 @@
+# Load packages
+library(tidyverse)
+library(haven) # package for loading .dta files
+library(tidyr)
+library(shinydashboard)
+library(shiny)
+library(shinyjs)
+library(DT)
+library(hrbrthemes)
