803a9dbe9e47e980786b5683d1df38d915556369
Luiza <luiza.c.andrade@gmail.com>
2021-03-25 20:54:38 -0400

    resolve #1

--- app/server.R
+++ app/server.R
@@ -1,166 +1,110 @@
 
 # Load packages --------------------------------------------------------------------------------
 
-library(shinydashboard)
-library(shiny)
-library(shinyjs)
-library(tidyverse)
-library(hrbrthemes)
-library(DT)
+  library(shinydashboard)
+  library(shiny)
+  library(shinyjs)
+  library(tidyverse)
+  library(hrbrthemes)
+  library(DT)
+  library(plotly)
 
 
 # Load data sets --------------------------------------------------------------------------------
 
   country_data <-
-    read_csv("data/mock_country.csv")
+    read_csv(file.path("data",
+                       "mock_country.csv"))
+
+  global_data <-
+    read_csv(file.path("data",
+                       "dtf_vars_global.rds"))
 
 # Server ---------------------------------------------------------------------------------------------
 
   server <- function(input, output, session) {
 
     output$dataset = renderDataTable({
-      country_data
+      global_data
     })
 
-    output$Overview <- renderPlot({
-      ggplot(data = country_data %>%
-               filter(tab == "Overview")) +
-        geom_segment(
-          aes(x = reorder(indicator,
-                          -value,
-                          sum),
-              xend = reorder(indicator,
-                             -value,
-                             sum),
-              y = 0,
-              yend = value,
-              color = category),
-          size = 1) +
-        geom_point(
-          aes(x = reorder(indicator,
-                          -value,
-                          sum),
-              y = value,
-              color = category),
-          size = 3)  +
-        coord_flip() +
-        scale_color_manual(values = c("#009E73", "#E69F00", "#D55E00")) +
-        theme_ipsum() +
-        theme(
-          panel.grid.minor.y = element_blank(),
-          panel.grid.major.y = element_blank(),
-          legend.position = "bottom",
-          legend.title = element_blank()
-        ) +
-        ylab("Distance to frontier") +
-        xlab("")
+    output$Overview <- renderPlotly({
+      plot <-
+        ggplot(data = country_data %>%
+                 filter(tab == "Overview")) +
+          geom_segment(
+            aes(x = reorder(indicator,
+                            -value,
+                            sum),
+                xend = reorder(indicator,
+                               -value,
+                               sum),
+                y = 0,
+                yend = value,
+                color = category),
+            size = 1) +
+          geom_point(
+            aes(x = reorder(indicator,
+                            -value,
+                            sum),
+                y = value,
+                color = category),
+            size = 3)  +
+          coord_flip() +
+          scale_color_manual(values = c("#009E73", "#E69F00", "#D55E00")) +
+          theme_ipsum() +
+          theme(
+            panel.grid.minor.y = element_blank(),
+            panel.grid.major.y = element_blank(),
+            legend.position = "bottom",
+            legend.title = element_blank()
+          ) +
+          ylab("Distance to frontier") +
+          xlab("")
 
-    })
-
-    output$Labor <- renderPlot({
-      ggplot(data = country_data %>%
-               filter(tab == "Labor")) +
-        geom_segment(
-          aes(x = reorder(indicator,
-                          -value,
-                          sum),
-              xend = reorder(indicator,
-                             -value,
-                             sum),
-              y = 0,
-              yend = value,
-              color = category),
-          size = 1) +
-        geom_point(
-          aes(x = reorder(indicator,
-                          -value,
-                          sum),
-              y = value,
-              color = category),
-          size = 3)  +
-        coord_flip() +
-        scale_color_manual(values = c("#009E73", "#E69F00", "#D55E00")) +
-        theme_ipsum() +
-        theme(
-          panel.grid.minor.y = element_blank(),
-          panel.grid.major.y = element_blank(),
-          legend.position = "bottom",
-          legend.title = element_blank()
-        ) +
-        ylab("Distance to frontier") +
-        xlab("")
+      ggplotly(plot)
 
     })
 
-    output$Law <- renderPlot({
-      ggplot(data = country_data %>%
-               filter(tab == "Legal")) +
-        geom_segment(
-          aes(x = reorder(indicator,
-                          -value,
-                          sum),
-              xend = reorder(indicator,
-                             -value,
-                             sum),
-              y = 0,
-              yend = value,
-              color = category),
-          size = 1) +
-        geom_point(
-          aes(x = reorder(indicator,
-                          -value,
-                          sum),
-              y = value,
-              color = category),
-          size = 3)  +
-        coord_flip() +
-        scale_color_manual(values = c("#009E73", "#E69F00", "#D55E00")) +
-        theme_ipsum() +
-        theme(
-          panel.grid.minor.y = element_blank(),
-          panel.grid.major.y = element_blank(),
-          legend.position = "bottom",
-          legend.title = element_blank()
-        ) +
-        ylab("Distance to frontier") +
-        xlab("")
+    output$Labor <- renderPlotly({
+      plot <-
+        ggplot(data = country_data %>%
+                 filter(tab == "Labor")) +
+          geom_segment(
+            aes(x = reorder(indicator,
+                            -value,
+                            sum),
+                xend = reorder(indicator,
+                               -value,
+                               sum),
+                y = 0,
+                yend = value,
+                color = category),
+            size = 1) +
+          geom_point(
+            aes(x = reorder(indicator,
+                            -value,
+                            sum),
+                y = value,
+                color = category),
+            size = 3)  +
+          coord_flip() +
+          scale_color_manual(values = c("#009E73", "#E69F00", "#D55E00")) +
+          theme_ipsum() +
+          theme(
+            panel.grid.minor.y = element_blank(),
+            panel.grid.major.y = element_blank(),
+            legend.position = "bottom",
+            legend.title = element_blank()
+          ) +
+          ylab("Distance to frontier") +
+          xlab("")
+
+      ggplotly(plot)
 
     })
 
-    output$Finance <- renderPlot({
-      ggplot(data = country_data %>%
-               filter(tab == "Finance")) +
-        geom_segment(
-          aes(x = reorder(indicator,
-                          -value,
-                          sum),
-              xend = reorder(indicator,
-                             -value,
-                             sum),
-              y = 0,
-              yend = value,
-              color = category),
-          size = 1) +
-        geom_point(
-          aes(x = reorder(indicator,
-                          -value,
-                          sum),
-              y = value,
-              color = category),
-          size = 3)  +
-        coord_flip() +
-        scale_color_manual(values = c("#009E73", "#E69F00", "#D55E00")) +
-        theme_ipsum() +
-        theme(
-          panel.grid.minor.y = element_blank(),
-          panel.grid.major.y = element_blank(),
-          legend.position = "bottom",
-          legend.title = element_blank()
-        ) +
-        ylab("Distance to frontier") +
-        xlab("")
 
-    })
 
   }
 
--- app/ui.R
+++ app/ui.R
@@ -5,9 +5,11 @@
   library(shinyjs)
   library(tidyverse)
   library(DT)
+  library(plotly)
 
   country_list <-
-    read_csv("data/wb_country_list.csv")
+    read_csv(file.path("data",
+                       "wb_country_list.csv"))
 
   ui <- dashboardPage(
 
@@ -25,15 +27,9 @@
                   menuItem("Country overview",
                            tabName = "overview",
                            icon = icon("globe")),
-                  menuItem("Finance",
-                           tabName = "fin",
-                           icon = icon("chart-line")),
                   menuItem("Labor",
                            tabName = "labor",
                            icon = icon("building")),
-                  menuItem("Law",
-                           tabName = "law",
-                           icon = icon("gavel")),
                   menuItem("Browse the data",
                            tabName = "data",
                            icon = icon("database"))
@@ -96,12 +92,13 @@
                         )
                 )
         ),
+
         tabItem(tabName = "overview",
                 box(solidHeader = TRUE,
                     width = 9,
                     title = "Country institutional assessment",
-                    plotOutput("Overview",
-                               width = "65%"),
+                    plotlyOutput("Overview",
+                                 width = "65%"),
                     h5("About the data and indicators"),
                     p("Lorem ipsum dolor sit amet, consectetur adipiscing elit,
                           sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
@@ -111,27 +108,13 @@
                           sunt in culpa qui officia deserunt mollit anim id est laborum.")
                 )
         ),
-        tabItem(tabName = "fin",
-               box(solidHeader = TRUE,
-                   width = 9,
-                   title = "Financial institutions",
-                   plotOutput("Finance",
-                            width = "65%"),
-                 h5("About the data and indicators"),
-                 p("Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-                          sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-                          quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                          Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-                          Excepteur sint occaecat cupidatat non proident,
-                          sunt in culpa qui officia deserunt mollit anim id est laborum.")
-               )
-        ),
+
         tabItem(tabName = "labor",
                 box(solidHeader = TRUE,
                     width = 9,
                     title = "Labor market institutions",
-                    plotOutput("Labor",
-                               width = "65%"),
+                    plotlyOutput("Labor",
+                                 width = "65%"),
                     h5("About the data and indicators"),
                     p("Lorem ipsum dolor sit amet, consectetur adipiscing elit,
                           sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
@@ -141,22 +124,7 @@
                           sunt in culpa qui officia deserunt mollit anim id est laborum.")
                 )
         ),
-        tabItem(tabName = "law",
-                box(solidHeader = TRUE,
-                    width = 9,
-                    title = "Legal institutions",
-                    plotOutput("Law",
-                               width = "65%"),
-                    h5("About the data and indicators"),
-                    p("Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-                          sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-                          quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                          Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-                          Excepteur sint occaecat cupidatat non proident,
-                          sunt in culpa qui officia deserunt mollit anim id est laborum.")
-                )
 
-        ),
         tabItem(tabName = "data",
                 box(solidHeader = TRUE,
                     width = 9,
