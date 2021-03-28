8b2494c31f24d965665b8b9ab288672342f891c8
Luiza <luiza.c.andrade@gmail.com>
2021-03-28 15:07:55 -0400

    export list of wb country groups

--- code/R/03-process-country-groups.R
+++ code/R/03-process-country-groups.R
@@ -17,17 +17,6 @@
                   "wb_country_list.csv")) %>%
     rename(country_name = country)
 
-  countries <-
-    country_list %>%
-    select(country_name,
-           country_code) %>%
-    unique
-
-  dtf_vars_global <-
-    read_rds(here("data",
-                  "data_cleaned",
-                  "dtf_vars_global.rds"))
-
   group_codes <-
     c("EUU",
       "HIC",
@@ -36,51 +25,22 @@
       "LMY",
       "MIC",
       "OED",
-      "NAC",
       "UMC")
-
-
-# 2 Create groups vars #########################################################################
-
-  group_vars <- function(country, code) {
-    group_contries <-
-      country_groups %>%
-      filter(group_code == code) %>%
-      select(country) %>%
-      pluck(1)
-
-    country %in% group_contries
-  }
-
-  countries <-
-    group_codes %>%
-    map(
-      ~ countries %>%
-        mutate(
-          !!paste0(.x) :=
-            group_vars(country_name, .x)
-        ) %>%
-        select(matches(.x))
-    ) %>%
-    bind_cols(countries, .)
-
-  dtf_vars_global <-
-    dtf_vars_global %>%
-    left_join(countries)
 
   groups <-
     country_list %>%
     filter(group_code %in% group_codes) %>%
     rename(group_name = group) %>%
-    select(group_name,
-           group_code)
+    select(group_code,
+           group_name) %>%
+    unique
 
 # 3 Save datasets ###########################################################################
 
-  write_rds(dtf_vars_global,
+  write_rds(country_list,
             here("app",
                  "data",
-                 "country_dtf.rds"))
+                 "wb_country_list.rds"))
 
   write_rds(groups,
             here("app",
