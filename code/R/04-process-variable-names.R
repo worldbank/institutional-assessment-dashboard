f69316179bcba14c06fb2a2bca9fbbed1184143f
gabrielvazdemelo <gabrielvazdemelo@gmail.com>
2021-03-29 12:41:41 -0300

    update process variable names

--- code/R/04-process-variable-names.R
+++ code/R/04-process-variable-names.R
@@ -8,98 +8,130 @@ packages <-
 pacman::p_load(packages,
                character.only = TRUE)
 
+# Data =======================================================================================
+data_selected <-
+  read_rds(here("data",
+                "data_cleaned",
+                "selected_vars.rds"))
+
+source(file.path("app/auxiliary",
+                 "vars-by-family.R"))
+
 # Inputs =====================================================================================
 variable_names <-
-  data.frame(
-    names = c(
-        # Family names
-        "vars_leg; Legal institutions",
-        "vars_pol; Political institutions",
-        "vars_publ; Public sector institutions",
-        "vars_social; Social institutions",
-        "vars_transp; Accountability institutions",
-        "vars_mkt; Business & trade institutions",
-        "vars_lab; Labor market institutions",
-        "vars_fin; Financial institutions",
-        "vars_service_del; Governance of SOEs",
-        # Variables
-        "centregov_mean; Centre of Government, influence",
-        "nontariff_barriers; Non-tariffs trade barriers",
-        "property_rights; Property rights",
-        "protection_incumbents; Protection of incumbents",
-        "complexity_procedures; Complex regulatory procedures",
-        "barriers_startups; Admin burdens on start-ups",
-        "barriers_trade_expl; Explicit barriers to trade",
-        "barriers_trade_oth; Other barriers to trade",
-        "regulatory_governance; Regulatory governance",
-        "open_data_barometer; Open Data Barometer",
-        "rigorous_impartial_pa; Rigorous and impartial PA",
-        "legaleff_challenging; Challenging regulations",
-        "legaleff_disputes; Settling disputes",
-        "insolvency_framework; Insolvency framework, strength",
-        "financial_institution; Financial Institutions",
-        "access_credit_overall; Getting credit",
-        "competition_rules_fin; Competition regulation",
-        "efficiency_superv_bank; Supervision efficiency-banking",
-        "efficiency_superv_fin; Supervision efficiency-financial",
-        "minimum_wage_ratio; Minimum to mean wage ratio",
-        "union_density; Union density",
-        "empl_protection_perm; Employment protection, regular",
-        "empl_protection_temp; Employment protection, temp.",
-        "collective_barg; Collective bargaining coverage",
-        "govreg_burden; Burden of gov. regulation",
-        "efw_labor_mkt_reg; Labor market regulations",
-        "wsj_financialfreedom; Financial freedom",
-        "wsj_businessfreedom; Business freedom",
-        "wsj_propertyrights; Property rights",
-        "efw_property_rights; Property rights protection",
-        "efw_reg_trade_barr; Regulatory trade barriers",
-        "efw_controls_movement; Control capital/people movs",
-        "efw_businessreg; Business regulations",
-        "efw_credit_mkt_reg; Credit market regulations",
-        "efw_free_foreign_curr; Freedom, foreign bank accounts",
-        "f3_security; Order and security",
-        "trust_pol; Public trust in politician",
-        "v2dlengage; Engaged society",
-        "f2_corruption; Absence of corruption",
-        "e_ti_cpi; Perception of corruption",
-        "f4_rights; Fundamental rights",
-        "v2lgqugen; Lower chamber gender quota",
-        "v2pepwrsoc; Power by social group",
-        "v2pepwrses; Power by socioeconomic position",
-        "f1_govpowers; Constraints on Gov. Powers",
-        "e_fh_pr; Political rights",
-        "e_fh_cl; Civil liberties",
-        "e_p_polity; Polity IV score",
-        "f8_criminaljustice; Criminal justice",
-        "f7_civiljustice; Civil justice",
-        "f6_regulatoryenf; Regulatory enforcement",
-        "f5_opengov; Open government",
-        "dskills_value_2018; Digital skills",
-        "onlineserviceindex; Egov, Online service",
-        "humancapitalindex; Egov, Human capital",
-        "telecom_infr; Egov, Telecom infr",
-        "prebid_score; Procurement-pre-bid",
-        "bid_submission_score; Procurement-submission",
-        "evaluation_score; Procurement-evaluation",
-        "contract_manag_score; Procurement-contract mgmt",
-        "perf_guarantee_score; Procurement-performance gntee",
-        "payments_score; Procurement-payment",
-        "proc_mean_score; Procurement score",
-        "val_direct_contracts_sh; Share of direct contracts (value)",
-        "wgi_voice_acc; Voice and accountability",
-        "wgi_pol_stability; Political stability",
-        "wgi_gov_effective; Government effectiveness",
-        "wgi_regulatory; Regulatory quality",
-        "wgi_rulelaw; Rule of law",
-        "wgi_control_corr; Control of corruption",
-        "v2x_cspart; Civil society participation",
-        "v2pepwrgen; Power by gender",
-        "cbi; Central bank independence"
+  data_selected %>%
+  select(
+    all_of(vars_all)
+  ) %>%
+  pivot_longer(
+    everything()
+  ) %>%
+  select(variable=name,-value) %>%
+  unique %>%
+  arrange(variable) %>%
+  mutate(
+    var_name =
+      case_when(
+        variable == "centregov_mean" ~ "Centre of Government, influence",
+        variable == "nontariff_barriers" ~ "Non-tariffs trade barriers",
+        variable == "property_rights" ~ "Property rights",
+        variable == "protection_incumbents" ~ "Protection of incumbents",
+        variable == "complexity_procedures" ~ "Complex regulatory procedures",
+        variable == "barriers_startups" ~ "Admin burdens on start-ups",
+        variable == "barriers_trade_expl" ~ "Explicit barriers to trade",
+        variable == "barriers_trade_oth" ~ "Other barriers to trade",
+        variable == "regulatory_governance" ~ "Regulatory governance",
+        variable == "open_data_barometer" ~ "Open Data Barometer",
+        variable == "rigorous_impartial_pa" ~ "Rigorous and impartial PA",
+        variable == "legaleff_challenging" ~ "Challenging regulations",
+        variable == "legaleff_disputes" ~ "Settling disputes",
+        variable == "insolvency_framework" ~ "Insolvency framework, strength",
+        variable == "financial_institution" ~ "Financial Institutions",
+        variable == "access_credit_overall" ~ "Getting credit",
+        variable == "competition_rules_fin" ~ "Competition regulation",
+        variable == "efficiency_superv_bank" ~ "Supervision efficiency-banking",
+        variable == "efficiency_superv_fin" ~ "Supervision efficiency-financial",
+        variable == "minimum_wage_ratio" ~ "Minimum to mean wage ratio",
+        variable == "union_density" ~ "Union density",
+        variable == "empl_protection_perm" ~ "Employment protection, regular",
+        variable == "empl_protection_temp" ~ "Employment protection, temp.",
+        variable == "collective_barg" ~ "Collective bargaining coverage",
+        variable == "govreg_burden" ~ "Burden of gov. regulation",
+        variable == "efw_labor_mkt_reg" ~ "Labor market regulations",
+        variable == "wsj_financialfreedom" ~ "Financial freedom",
+        variable == "wsj_businessfreedom" ~ "Business freedom",
+        variable == "wsj_propertyrights" ~ "Property rights",
+        variable == "efw_property_rights" ~ "Property rights protection",
+        variable == "efw_reg_trade_barr" ~ "Regulatory trade barriers",
+        variable == "efw_controls_movement" ~ "Control capital/people movs",
+        variable == "efw_businessreg" ~ "Business regulations",
+        variable == "efw_credit_mkt_reg" ~ "Credit market regulations",
+        variable == "efw_free_foreign_curr" ~ "Freedom, foreign bank accounts",
+        variable == "f3_security" ~ "Order and security",
+        variable == "trust_pol" ~ "Public trust in politician",
+        variable == "v2dlengage" ~ "Engaged society",
+        variable == "f2_corruption" ~ "Absence of corruption",
+        variable == "e_ti_cpi" ~ "Perception of corruption",
+        variable == "f4_rights" ~ "Fundamental rights",
+        variable == "v2lgqugen" ~ "Lower chamber gender quota",
+        variable == "v2pepwrsoc" ~ "Power by social group",
+        variable == "v2pepwrses" ~ "Power by socioeconomic position",
+        variable == "f1_govpowers" ~ "Constraints on Gov. Powers",
+        variable == "e_fh_pr" ~ "Political rights",
+        variable == "e_fh_cl" ~ "Civil liberties",
+        variable == "e_p_polity" ~ "Polity IV score",
+        variable == "f8_criminaljustice" ~ "Criminal justice",
+        variable == "f7_civiljustice" ~ "Civil justice",
+        variable == "f6_regulatoryenf" ~ "Regulatory enforcement",
+        variable == "f5_opengov" ~ "Open government",
+        variable == "dskills_value_2018" ~ "Digital skills",
+        variable == "onlineserviceindex" ~ "Egov, Online service",
+        variable == "humancapitalindex" ~ "Egov, Human capital",
+        variable == "telecom_infr" ~ "Egov, Telecom infr",
+        variable == "prebid_score" ~ "Procurement-pre-bid",
+        variable == "bid_submission_score" ~ "Procurement-submission",
+        variable == "evaluation_score" ~ "Procurement-evaluation",
+        variable == "contract_manag_score" ~ "Procurement-contract mgmt",
+        variable == "perf_guarantee_score" ~ "Procurement-performance gntee",
+        variable == "payments_score" ~ "Procurement-payment",
+        variable == "proc_mean_score" ~ "Procurement score",
+        variable == "val_direct_contracts_sh" ~ "Share of direct contracts (value)",
+        variable == "wgi_voice_acc" ~ "Voice and accountability",
+        variable == "wgi_pol_stability" ~ "Political stability",
+        variable == "wgi_gov_effective" ~ "Government effectiveness",
+        variable == "wgi_regulatory" ~ "Regulatory quality",
+        variable == "wgi_rulelaw" ~ "Rule of law",
+        variable == "wgi_control_corr" ~ "Control of corruption",
+        variable == "v2x_cspart" ~ "Civil society participation",
+        variable == "v2pepwrgen" ~ "Power by gender",
+        variable == "cbi" ~ "Central bank independence"
+      ),
+    family_var =
+      case_when(
+        variable %in% vars_leg ~ "vars_leg",
+        variable %in% vars_pol ~ "vars_pol",
+        variable %in% vars_publ ~ "vars_publ",
+        variable %in% vars_social ~ "vars_social",
+        variable %in% vars_transp ~ "vars_transp",
+        variable %in% vars_mkt ~ "vars_mkt",
+        variable %in% vars_lab ~ "vars_lab",
+        variable %in% vars_fin ~ "vars_fin",
+        variable %in% vars_service_del ~ "vars_service_del"
+      ),
+    family_name =
+      case_when(
+        variable %in% vars_leg ~ "Legal institutions",
+        variable %in% vars_pol ~ "Political institutions",
+        variable %in% vars_publ ~ "Public sector institutions",
+        variable %in% vars_social ~ "Social institutions",
+        variable %in% vars_transp ~ "Accountability institutions",
+        variable %in% vars_mkt ~ "Business & trade institutions",
+        variable %in% vars_lab ~ "Labor market institutions",
+        variable %in% vars_fin ~ "Financial institutions",
+        variable %in% vars_service_del ~ "Governance of SOEs"
       )
   ) %>%
-  tidyr::separate(names, into = c("variable","name_variable"), sep=";")
-
+  arrange(family_var)
 
 # Save datasets ###########################################################################
 
@@ -107,3 +139,4 @@ variable_names <-
             here("app",
                  "auxiliary",
                  "variable_names.rds"))
+
