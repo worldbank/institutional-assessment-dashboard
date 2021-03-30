28061b984578e457634a063500fbab8832c5e1e9
Luiza <luiza.c.andrade@gmail.com>
2021-03-30 09:27:50 -0400

    country group definitions

--- app/ui.R
+++ app/ui.R
@@ -52,9 +52,10 @@
       ),
 
       tabItems(
+
+      # Home tab ----------------------------------------------------------------------------------
         tabItem(tabName = "home",
 
-  # About ----------------------------------------------------------------------------------------------------------
                 fluidRow(
                   box(
                     solidHeader = TRUE,
@@ -67,8 +68,6 @@
                     p("Lorem ipsum dolor sit amet, consectetur adipiscing elit.")
                   )
                 ),
-
-  # Country selection ----------------------------------------------------------------------------------------------
 
                 fluidRow(
                   box(
@@ -130,89 +129,134 @@
 
         ),
 
-        tabItem(tabName = "overview",
-                box(solidHeader = TRUE,
-                    width = 9,
-                    title = "Country institutional assessment",
-                    plotlyOutput("Overview",
-                                 width = "65%"),
-                    h5("About the data and indicators"),
-                    p("Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-                          sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-                          quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                          Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-                          Excepteur sint occaecat cupidatat non proident,
-                          sunt in culpa qui officia deserunt mollit anim id est laborum.")
-                )
+      # Country overview tab -----------------------------------------------------------------------
+      tabItem(tabName = "overview",
+              box(solidHeader = TRUE,
+                  width = 9,
+                  title = "Country institutional assessment",
+                  plotlyOutput("Overview",
+                               width = "65%"),
+                  h5("About the data and indicators"),
+                  p("Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                        sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                        Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+                        Excepteur sint occaecat cupidatat non proident,
+                        sunt in culpa qui officia deserunt mollit anim id est laborum.")
+              )
+      ),
+
+      # Labor market institutions tab --------------------------------------------------------------
+      tabItem(tabName = "labor",
+              box(solidHeader = TRUE,
+                  width = 9,
+                  title = "Labor market institutions",
+                  plotlyOutput("Labor",
+                               width = "65%"),
+                  h5("About the data and indicators"),
+                  p("Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                        sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                        Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+                        Excepteur sint occaecat cupidatat non proident,
+                        sunt in culpa qui officia deserunt mollit anim id est laborum.")
+              )
+      ),
+
+      # Data tab ----------------------------------------------------------------------------------
+      tabItem(tabName = "data",
+
+              box(solidHeader = TRUE,
+                  width = 3,
+                  title = "Select countries to display",
+                  collapsible = TRUE,
+                  collapsed = TRUE,
+                  selectInput(
+                    "vars",
+                    label = NULL,
+                    choices = c("All",
+                                "Current comparison"
+                    ),
+                    selected = 1,
+                    width = "100%"
+                  )
+              ),
+
+              box(solidHeader = TRUE,
+                  width = 9,
+                  title = "Select indicators to display",
+                  collapsible = TRUE,
+                  collapsed = TRUE,
+                  checkboxGroupInput(
+                    "vars",
+                    label = NULL,
+                    choiceNames = c("Governance of SOEs",
+                                    "Accountability institutions",
+                                    "Business & trade institutions",
+                                    "Financial institutions",
+                                    "Labor market institutions",
+                                    "Legal institutions",
+                                    "Political institutions",
+                                    "Public sector institutions  ",
+                                    "Social institutions"
+                                    ),
+                    choiceValues = family_names,
+                    inline = TRUE,
+                    selected = family_names,
+                    width = "100%"
+                  )
+              ),
+
+
+
+              box(solidHeader = TRUE,
+                  width = 12,
+                  title = "Browse the data",
+                  dataTableOutput("dataset")
+              )
         ),
 
-        tabItem(tabName = "labor",
-                box(solidHeader = TRUE,
-                    width = 9,
-                    title = "Labor market institutions",
-                    plotlyOutput("Labor",
-                                 width = "65%"),
-                    h5("About the data and indicators"),
-                    p("Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-                          sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-                          quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                          Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-                          Excepteur sint occaecat cupidatat non proident,
-                          sunt in culpa qui officia deserunt mollit anim id est laborum.")
-                )
-        ),
+      # Methodology tab ---------------------------------------------------------------------------
+      tabItem(tabName = "methodology",
 
-        tabItem(tabName = "data",
-
-                box(solidHeader = TRUE,
-                    width = 3,
-                    title = "Select countries to display",
-                    collapsible = TRUE,
-                    collapsed = TRUE,
-                    selectInput(
-                      "vars",
-                      label = NULL,
-                      choices = c("All",
-                                  "Current comparison"
+              box(solidHeader = TRUE,
+                  width = 9,
+                  title = "Country group definitions",
+                  collapsible = TRUE,
+                  collapsed = TRUE,
+                  p(
+                    paste(
+                      "Country group definitions are extracted from the",
+                      a(
+                        "World Bank Country and Lending Groups.",
+                        href = "https://datahelpdesk.worldbank.org/knowledgebase/articles/906519-world-bank-country-and-lending-groups"
                       ),
-                      selected = 1,
-                      width = "100%"
+                      "It classifies all 189 World Bank member countries, and all other economies with populations of more than 30,000.",
+                      "Economies are divided among income groups according to 2019 gross national income (GNI) per capita,",
+                      "calculated using the World Bank Atlas method."
                     )
-                ),
-
-                box(solidHeader = TRUE,
-                    width = 9,
-                    title = "Select indicators to display",
-                    collapsible = TRUE,
-                    collapsed = TRUE,
-                    checkboxGroupInput(
-                      "vars",
-                      label = NULL,
-                      choiceNames = c("Governance of SOEs",
-                                      "Accountability institutions",
-                                      "Business & trade institutions",
-                                      "Financial institutions",
-                                      "Labor market institutions",
-                                      "Legal institutions",
-                                      "Political institutions",
-                                      "Public sector institutions  ",
-                                      "Social institutions"
-                                      ),
-                      choiceValues = family_names,
-                      inline = TRUE,
-                      selected = family_names,
-                      width = "100%"
-                    )
-                ),
-
-
-
-                box(solidHeader = TRUE,
-                    width = 12,
-                    title = "Browse the data",
-                    dataTableOutput("dataset")
-                )
-        )
+                  )
+                  # ),
+                  #         "The groups are:"
+                  #         tags$ul(
+                  #           tags$li("<b>Low income:</b> $1,035 or less"),
+                  #           tags$li("<b>Lower middle income:</b> $1,036 - 4,045"),
+                  #           tags$li("<b>Upper middle income:</b> $4,046 - 12,535"),
+                  #           tags$li("<b>High income:</b> $12,536 or more")
+                  #         ),
+                  #         "The term <i>country</i>, used interchangeably with <i>economy,</i>",
+                  #         "does not imply political independence but refers to any territory for
+                  #         which authorities report separate social or economic statistics.",
+                  #         "Income classifications set on 1 July 2020 remain in effect until 1 July 2021.",
+                  #         "Argentina, which was temporarily unclassified in July 2016 pending release of revised national accounts statistics,",
+                  #         "was classified as upper middle income for FY17 as of 29 September 2016 based on alternative conversion factors.")
+                  # )
+              )
       )
-    )
-  )
+
+
+    # Closing parenthesis -------------------------------------------------------------------------
+
+      ) # Closing tab item
+    ) # Closing dashboard body
+  ) # closing dashboard page
