fc4941f5b0f74409bfdff5a53bdae75192acf62d
Luiza <luiza.c.andrade@gmail.com>
2021-04-08 16:43:01 -0400

    Revert "minor update to UI"
    This reverts commit da102211904e4fbb06b999bb380c724319a3ba36.

--- app/ui.R
+++ app/ui.R
@@ -20,8 +20,7 @@
 
   variable_names <-
     read_rds(file.path("data",
-                       "variable_names.rds")) %>%
-    arrange(var_name)
+                       "variable_names.rds"))
 
   source(file.path("auxiliary",
                    "vars-by-family.R"))
@@ -352,7 +351,7 @@
                     selectInput(
                       "vars_map",
                       label = NULL,
-                      choices = c("", variable_names$var_name),
+                      choices = c("",sort(variable_names$var_name)),
                       width = "100%"
                     )
                 ),
@@ -361,6 +360,13 @@
                     width = box_width,
                     title = "Map",
                     leafletOutput("map_plot", height="600")
+                    #h5("About the data and indicators"),
+                    #p("Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                    #                        sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                    #                        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                    #                        Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+                    #                        Excepteur sint occaecat cupidatat non proident,
+                    #                        sunt in culpa qui officia deserunt mollit anim id est laborum.")
                 )
         ),
 
