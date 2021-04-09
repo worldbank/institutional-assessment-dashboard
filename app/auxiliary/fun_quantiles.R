2d8d2c2bcb3078838a638074a3cab244ce05b3e8
Luiza <luiza.c.andrade@gmail.com>
2021-04-08 21:18:13 -0400

    update quantile function

--- app/auxiliary/fun_quantiles.R
+++ app/auxiliary/fun_quantiles.R
@@ -2,14 +2,15 @@
 # FUNCTION THAT DEFINES THE QUANTILES BASED ON SELECTED COUNTRY AND COMPARISON GROUP -----------------------
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 
-def_quantiles <- function(data, selected_country, selected, vars) {
+def_quantiles <- function(data, base_country, comparison_countries, vars) {
 
     quantiles <-
       data %>%
       filter(
-        country_name %in% selected
+        country_name %in% comparison_countries
       ) %>%
-      select(country_name,all_of(vars)) %>%
+      select(country_name,
+             all_of(vars)) %>%
       summarise_at(vars(vars),
                    ~ quantile(., c(0.25, 0.5), na.rm = TRUE)) %>%
     t %>%
@@ -21,7 +22,7 @@ def_quantiles <- function(data, selected_country, selected, vars) {
 
   data <-
     data %>%
-    filter(country_name == selected_country) %>%
+    filter(country_name == base_country) %>%
     select(all_of(vars)) %>%
     pivot_longer(all_of(vars)) %>%
     rename(variable = name,
@@ -29,9 +30,9 @@ def_quantiles <- function(data, selected_country, selected, vars) {
     left_join(quantiles) %>%
     mutate(
       classification = case_when(
+        dtf >= q50 ~ "Advanced",
         dtf <= q25 ~ "Weak",
-        dtf > q25 & dtf <= q50 ~ "Emerging",
-        dtf > q50 ~ "Advanced"
+        dtf > q25 & dtf < q50 ~ "Emerging"
       )
     ) %>%
     filter(!is.na(dtf))
