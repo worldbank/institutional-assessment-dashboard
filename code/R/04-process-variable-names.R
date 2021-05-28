12969ac18dd5fbc0694c2aeb92dd3b0aa0574471
Gabriel Vaz de Melo <gabrielvazdemelo@gmail.com>
2021-05-28 09:56:32 -0300

    fix family names for map plot

--- code/R/04-process-variable-names.R
+++ code/R/04-process-variable-names.R
@@ -146,10 +146,10 @@ variable_names <-
         variable == "vars_publ" ~ "Public sector institutions",
         variable == "vars_social" ~ "Social institutions",
         variable == "vars_transp" ~ "Accountability institutions",
-        variable == "vars_mkt" ~ "Business & trade institutions",
+        variable == "vars_mkt" ~ "Business environment and trade institutions",
         variable == "vars_lab" ~ "Labor market institutions",
-        variable == "vars_fin" ~ "Financial institutions",
-        variable == "vars_service_del" ~ "Governance of SOEs"
+        variable == "vars_fin" ~ "Financial market institutions",
+        variable == "vars_service_del" ~ "Institutions for service delivery"
       ),
     family_var =
       case_when(
@@ -172,7 +172,7 @@ variable_names <-
         variable %in% vars_transp | variable == "vars_transp" ~ "Accountability institutions",
         variable %in% vars_mkt | variable == "vars_mkt" ~ "Business environment and trade institutions",
         variable %in% vars_lab | variable == "vars_lab" ~ "Labor market institutions",
-        variable %in% vars_fin | variable == "vars_fin" ~ "Financial institutions",
+        variable %in% vars_fin | variable == "vars_fin" ~ "Financial market institutions",
         variable %in% vars_service_del | variable == "vars_service_del" ~ "Institutions for service delivery"
       )
   ) %>%
