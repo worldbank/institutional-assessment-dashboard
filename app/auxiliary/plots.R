5565e07cf98564bee5234a11b05f33ebf135bc70
Gabriel Vaz de Melo <gabrielvazdemelo@gmail.com>
2021-06-11 18:44:38 -0300

    fix caption margin and legend position

--- app/auxiliary/plots.R
+++ app/auxiliary/plots.R
@@ -64,7 +64,10 @@
       x %>%
         ggplotly(tooltip = "text") %>%
         layout(
-          margin = list(l=50, r=50, t=75, b=140),
+          legend = list(
+            y=0.5
+          ),
+          margin = list(l=50, r=50, t=75, b=180),
           annotations =
             list(x = 0, y = -0.475,
                  text = paste0("<b>Notes:</b> ", y, " compared to ", paste(z, collapse = ", "), ".",
