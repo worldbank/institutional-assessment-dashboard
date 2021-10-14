d06164c9ad8f3926438a0ec8e04a7ccbfb0453cf
Gabriel Vaz de Melo <gabrielvazdemelo@gmail.com>
2021-10-14 19:23:53 -0300

    update quantiles function with new plot

--- app/auxiliary/fun_quantiles.R
+++ app/auxiliary/fun_quantiles.R
@@ -4,62 +4,43 @@
 
 def_quantiles <- function(data, base_country, comparison_countries, vars) {
 
-    quantiles <-
-      data %>%
-      ungroup() %>%
-      filter(
-        country_name == base_country | country_name %in% comparison_countries
-      ) %>%
-      select(all_of(vars)) %>%
-      summarise(
-        across(
-          all_of(vars),
-          list(
-            q25 = ~ quantile(., c(0.25), na.rm = TRUE),
-            q50 = ~ quantile(., c(0.5), na.rm = TRUE),
-            avg = ~ mean(., na.rm = TRUE)
-          ),
-          .names="{.col}-{.fn}"
-        )
-      ) %>%
-      pivot_longer(
-        everything(),
-        names_to = c("variable","fn"),
-        names_sep = "-"
-      ) %>%
-      pivot_wider(
-        id_cols = "variable",
-        names_from = "fn"
-      )
+  #vars <- vars_lab
 
-  quantiles_group <- as.data.frame(apply(data[all_of(vars)], 2, quantile, probs = c(0.25,0.5) , na.rm = T)) %>%
-    rownames_to_column(var="quantile_break") %>%
-    pivot_longer(all_of(vars)) %>%
-    rename(variable=name,dtf_q=value) %>%
-    mutate(
-      quantile_break = case_when(
-        quantile_break == "25%" ~ "q25_group",
-        quantile_break == "50%" ~ "q50_group"
-      )
-    )
-
-  data <-
+  quantiles <-
     data %>%
-    filter(country_name == base_country) %>%
-    select(all_of(vars)) %>%
-    pivot_longer(all_of(vars)) %>%
-    rename(variable = name,
-           dtf = value) %>%
-    left_join(quantiles) %>%
-    mutate(
-      classification = case_when(
-        dtf >= q50 ~ "Advanced",
-        dtf <= q25 ~ "Weak",
-        dtf > q25 & dtf < q50 ~ "Emerging"
-      )
+    ungroup() %>%
+    mutate(oecd = ifelse(country_code == "COL", 1, oecd)) %>%
+    filter(
+      lac6 == 1 | oecd == 1 | country_name == base_country
     ) %>%
-    filter(!is.na(dtf))
-
-  return(data)
+    select(country_name, all_of(vars), lac6, oecd, structural) %>%
+    pivot_longer(cols = vars,
+                 names_to = "variable") %>%
+    left_join(variable_names,
+              by = "variable") %>%
+    group_by(country_name, var_name, lac6, oecd, structural) %>%
+    summarise(dtf = mean(value, na.rm = TRUE))  %>%
+    group_by(var_name) %>%
+    mutate(
+      rank = dense_rank(dtf),
+      rank_max = max(rank, na.rm = TRUE),
+      r25 = quantile(1:rank_max, c(.25)),
+      r50 = quantile(1:rank_max, c(.5)),
+      dtt = (rank/rank_max),
+      status_dtt = case_when(
+        rank <= r25 ~ "Weak",
+        rank > r25 & rank <= r50 ~ "Emerging",
+        rank > r50 ~ "Advanced"
+      ),
+      q25 = quantile(dtf, c(0.25), na.rm = TRUE),
+      q50 = quantile(dtf, c(0.5), na.rm = TRUE),
+      avg = mean(dtf, na.rm = TRUE),
+      status_dtf = case_when(
+        dtf <= q25 ~ "Weak",
+        dtf > q25 & dtf <= q50 ~ "Emerging",
+        dtf > q50 ~ "Advanced"
+      ),
+      var_name = var_name %>% str_to_sentence() %>% str_replace_all("Soe", "SOE")
+    )
 
 }
