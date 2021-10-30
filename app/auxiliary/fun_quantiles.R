793c932096cf9c2ebf86f138971d3c0a19fbff0c
Gabriel Vaz de Melo <gabrielvazdemelo@gmail.com>
2021-10-30 20:12:02 -0300

    fix typo

--- app/auxiliary/fun_quantiles.R
+++ app/auxiliary/fun_quantiles.R
@@ -2,7 +2,7 @@
 # FUNCTION THAT DEFINES THE QUANTILES BASED ON SELECTED COUNTRY AND COMPARISON GROUP -----------------------
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 
-def_quantiles <- function(datam, base_country, country_list, selected_groups, vars, variable_names) {
+def_quantiles <- function(data, base_country, country_list, selected_groups, vars, variable_names) {
 
   #base_country <- "Uruguay"
 
