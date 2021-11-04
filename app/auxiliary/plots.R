e8adc7a2fc30f2e36b2f957b775b2e2967ec0219
Gabriel Vaz de Melo <gabrielvazdemelo@gmail.com>
2021-11-04 00:57:09 -0300

    update plot with old version with ctf on x and without medians

--- app/auxiliary/plots.R
+++ app/auxiliary/plots.R
@@ -1,61 +1,159 @@
-
+#data <- quantiles_group
 
 static_plot <-
   function(data,
-           selected_country,
+           base_country,
            tab_name) {
 
-    ggplot(data) +
-      geom_segment(
-        aes(x = reorder(var_name,
-                        -dtf),
-            xend = reorder(var_name,
-                           -dtf),
-            y = 0,
-            yend = dtf,
-            color = classification),
-        size = 1) +
+    order <-
+      data %>%
+      filter(country_name == base_country) %>%
+      arrange(dtt) %>%
+      select(var_name) %>%
+      unique %>%
+      unlist
+
+    data$var_name = factor(data$var_name,
+                           levels = order,
+                           ordered = TRUE)
+
+    bar_colors_dtf <-
+      data %>%
+      transmute(var_name = var_name,
+                Weak = q25,
+                Emerging = q50 - q25,
+                Advanced = 1 - q50) %>%
+      unique %>%
+      pivot_longer(cols = c(Weak, Emerging, Advanced),
+                   names_to = "status",
+                   values_to = "dtf")
+
+    bar_colors_dtt <-
+      data %>%
+      transmute(var_name = var_name,
+                Weak = .25,
+                Emerging = .25,
+                Advanced = .50) %>%
+      unique %>%
+      pivot_longer(cols = c(Weak, Emerging, Advanced),
+                   names_to = "status",
+                   values_to = "dtt")
+
+    colors <-
+      c("Weak" = "#D2222D",
+        "Emerging" = "#FFBF00",
+        "Advanced" = "#238823"
+      )
+
+    data_group <- data %>%
+      filter(!is.na(group)) %>%
+      group_by(group, var_name) %>%
+      summarise(dtt = median(dtt, na.rm = TRUE)) %>%
+      mutate(group_med = paste0(group," Median"))
+
+    shapes <- c(
+      "High income Median" = 21,
+      "Latin America & Caribbean Median" = 22,
+      "OECD members Median" = 23,
+      "European Union Median" = 24,
+      "Low income Median" = 25
+    )
+
+    ggplot() +
+      #geom_col(
+      #  data = bar_colors_dtt,
+      #  aes(y = var_name,
+      #      x = dtt,
+      #      fill = status),
+      #  width = .2,
+      #  alpha = .6
+      #) +
+      #geom_point(
+      #  data = data_group,
+      #  aes(y = var_name,
+    #      x = dtt,
+    #      shape = group_med),
+    #  alpha = .5,
+    #  color = "black",
+    #  fill = "white",
+    #  size = 4
+    #) +
+    geom_col(
+      data = bar_colors_dtf,
+      aes(y = var_name,
+          x = dtf,
+          fill = status),
+      width = .3,
+      alpha = .6
+    ) +
       geom_point(
-        aes(x = reorder(var_name,
-                        -dtf),
-            y = dtf,
-            text = map(paste(
-              '<b>Country:</b>', selected_country, '<br>',
-              '<b>Closeness to frontier:</b>', round(dtf, digits = 3), '<br>',
-              '<b>Classification:</b>', classification), HTML),
-            color = classification),
-        size = 3)  +
-      geom_point(
-        aes(x = reorder(var_name,
-                        -dtf),
-            y = avg,
-            text = map(paste(
-              '<b>CTF average:</b>', round(avg, digits = 3), '<br>'), HTML),),
-        size = 2,
-        shape = 18,
-        color = "black") +
-      coord_flip() +
+        data = data %>% filter(country_name == base_country),
+        aes(y = var_name,
+            x = dtt,
+            fill = status_dtf),
+        size = 6,
+        shape = 21,
+        color = "gray0"
+      ) +
+      geom_vline(
+        xintercept = 1,
+        linetype = "dashed",
+        color = colors["Advanced"],
+        size = 1
+      ) +
+      theme_minimal() +
+      theme(legend.position = "top",
+            panel.grid.minor = element_blank(),
+            panel.grid.major = element_blank(),
+            axis.ticks = element_blank(),
+            axis.text = element_text(color = "black"),
+            axis.text.y = element_text(size = 12),
+            axis.text.x = element_text(size = 11),
+            legend.box = "vertical") +
+      labs(y = NULL,
+           #x = NULL,
+           x = "CTF",
+           fill = NULL,
+           shape = NULL) +
+      scale_shape_manual(
+        values = shapes
+      )+
+      scale_fill_manual(
+        values = colors
+      ) +
       scale_color_manual(
-        values =
-          c("Advanced"="#009E73",
-            "Emerging"="#E69F00",
-            "Weak"="#D55E00"
-          )
+        values = colors
       ) +
-      scale_y_continuous(
-        limits = c(0,1)
-      )  +
-      guides(color = guide_legend(order=1)) +
-      theme_ipsum() +
-      theme(
-        panel.grid.minor.y = element_blank(),
-        panel.grid.major.y = element_blank(),
-        legend.position = "bottom",
-        legend.title = element_blank()
+      guides(
+        fill = guide_legend(ncol = 3),
+        shape = guide_legend(ncol = 3)
       ) +
-      ylab("Closeness to frontier") +
-      xlab("") +
-      labs(title = paste0(tab_name))
+      #scale_x_continuous(breaks = c(0, 0.5, 1),
+      #                   labels = c("Worst ranked",
+      #                              "Middle of ranking",
+      #                              "Top ranked")) +
+      #annotate(
+      #  geom = "text",
+      #  size = 3,
+      #  x = .13,
+      #  y = .65,
+      #  label = "Bottom 25%"
+      #) +
+    #annotate(
+    #  geom = "text",
+    #  size = 3,
+    #  x = .4,
+    #  y = .65,
+    #  label = "25% - 50%"
+    #) +
+    #annotate(
+    #  geom = "text",
+    #  size = 3,
+    #  x = .75,
+    #  y = .65,
+    #  label = "Top 50%"
+    #) +
+    labs(title = paste0(tab_name))
 
   }
 
@@ -66,17 +164,17 @@ interactive_plot <-
       layout(
         margin = list(l=50, r=50, t=75, b=140),
         annotations =
-          list(x = 0, y = -0.475,
+          list(x = 0, y = -0.4,
                text = paste0("<b>Notes:</b> ", y, " compared to ", paste(z, collapse = ", "), ".",
-                             "<br>Black squares show the group average.",
-                             "<br>Closeness to frontier is calculated as (worst-y)/(worst-frontier).",
-                             "<br>1 identifies the best performer and 0 the worst performer.",
+                             #"<br>Black squares show the group average.",
+                             #"<br>Closeness to frontier is calculated as (worst-y)/(worst-frontier).",
+                             #"<br>1 identifies the best performer and 0 the worst performer.",
                              "<br>Weak = bottom 25%; Emerging = 25%-50%; Advanced = top 50%."),
                showarrow = F,
                xref = 'paper',
                yref = 'paper',
                align = 'left',
-               font = list(size = 12)
+               font = list(size = 13)
           )
       ) %>%
       config(
@@ -90,7 +188,9 @@ interactive_plot <-
                                    "hoverClosest3d",
                                    "hoverClosestCartesian",
                                    "hoverCompareCartesian"),
-        toImageButtonOptions= list(filename = paste0(tolower(stringr::str_replace_all(tab_name,"\\s","_"))))
+        toImageButtonOptions= list(filename = paste0(tolower(stringr::str_replace_all(tab_name,"\\s","_"))),
+                                   width = 1100,
+                                   height =  1000)
       )
 
 
@@ -135,4 +235,5 @@ interactive_map <-
       )
 
   }
+
 
