5baacab893f956f4d510dc9d97e944231951c2fa
Gabriel Vaz de Melo <gabrielvazdemelo@gmail.com>
2021-12-12 22:12:06 -0300

    creates joint scripts for preparing data

---
+++ code/R/00-prepare-data-complete.R
@@ -0,0 +1,16 @@
+# Import variables data and variables control----
+source(file.path("app/auxiliary",
+                 "vars-control.R"))
+
+# Import original data ----
+source(file.path("code/R",
+                 "01-1-import-original-data.R"))
+
+# Import API data ----
+source(file.path("code/R",
+                 "01-2-import-api-data.R"))
+
+# Import API data ----
+source(file.path("code/R",
+                 "01-3-import-additional-data.R"))
+
