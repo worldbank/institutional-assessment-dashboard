ba684449880db78feb556d0a8889558f326a899b
Gabriel Vaz de Melo <gabrielvazdemelo@gmail.com>
2022-01-19 23:38:31 -0300

    bind data from all data sources

--- code/R/00-prepare-data-complete.R
+++ code/R/00-prepare-data-complete.R
@@ -14,3 +14,39 @@ source(file.path("code/R",
 source(file.path("code/R",
                  "01-3-import-additional-data.R"))
 
+data_select_original <-
+  data_cleaned %>%
+  select(
+    country_code,
+    country_name,
+    year,
+    all_of(vars_original)
+  )
+
+data_select_additions <-
+  additions %>%
+  select(
+    country_code,
+    year,
+    all_of(vars_additions)
+  )
+
+data_select_api <-
+  data_api %>%
+  select(
+    country_code,
+    year,
+    all_of(vars_api)
+  )
+
+data_binded <- data_select_original %>%
+  left_join(
+    data_select_additions,
+    by = c("country_code","year")
+  ) %>%
+  left_join(
+    data_select_api,
+    by = c("country_code","year")
+  )
+
+
