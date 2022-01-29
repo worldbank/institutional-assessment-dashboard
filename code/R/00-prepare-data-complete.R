bde34f4d5ff78a9957df277001dcd9b5a5655df8
Gabriel Vaz de Melo <gabrielvazdemelo@gmail.com>
2022-01-29 02:20:40 -0300

    update order of scripts from script 0

--- code/R/00-prepare-data-complete.R
+++ code/R/00-prepare-data-complete.R
@@ -4,25 +4,33 @@ source(file.path("app/auxiliary",
 
 # Import original data =====================================================
 source(file.path("code/R",
-                 "01-1-import-original-data.R"))
+                 "01-import-original-data.R"))
 
 # Import API data ==========================================================
 source(file.path("code/R",
-                 "01-2-import-api-data.R"))
+                 "02-import-api-data.R"))
 
 # Import additional data ===================================================
 source(file.path("code/R",
-                 "01-3-import-additional-data.R"))
+                 "03-import-additional-data.R"))
 
 # Bind all data sources ====================================================
 source(file.path("code/R",
-                 "01-4-bind-datasets.R"))
+                 "04-bind-datasets.R"))
 
 # Calculate CTF ============================================================
 source(file.path("code/R",
-                 "02-indicator-calc.R"))
+                 "05-indicator-calc.R"))
 
 # Country groups ===========================================================
 source(file.path("code/R",
-                 "03-process-country-groups.R"))
+                 "06-process-country-groups.R"))
+
+# Create Layer for Map ===========================================================
+source(file.path("code/R",
+                 "07-create-layer-map.R"))
+
+rm(list = ls(all.names = TRUE))
+gc()
+
 
