5dfb5cf0aab342086bfbf8500eafeb83f4fc330d
Gabriel Vaz de Melo <gabrielvazdemelo@gmail.com>
2022-01-30 16:36:06 -0300

    replace nan values (no indicator for one family) by na

--- app/auxiliary/fun_family_data.R
+++ app/auxiliary/fun_family_data.R
@@ -27,6 +27,9 @@ family_data <- function(data, base_country, variable_names) {
               by = "variable") %>%
     group_by(country_name, family_var) %>%
     summarise(value = mean(value, na.rm = TRUE)) %>%
+    mutate(
+      value = ifelse(is.nan(value),NA,value)
+    ) %>%
     pivot_wider(names_from = family_var)
 
 
