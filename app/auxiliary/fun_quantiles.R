2f7e772ae43d6562dfbf9eb6a5ef03ecf5af0800
Luiza <luiza.c.andrade@gmail.com>
2022-03-03 10:28:03 -0500

    refactoring quantiles function

--- app/auxiliary/fun_quantiles.R
+++ app/auxiliary/fun_quantiles.R
@@ -1,73 +1,90 @@
-# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 # FUNCTION THAT DEFINES THE QUANTILES BASED ON SELECTED COUNTRY AND COMPARISON GROUP -----------------------
-# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 
 def_quantiles <- function(data, base_country, country_list, comparison_countries, vars, variable_names) {
 
-
-  print(comparison_countries)
+# List all relevant countries
   comparison_list <-
     country_list %>%
     filter(country_name %in% comparison_countries)
 
+# List all variables that are missing for the base country -- these will be removed from the data
   na_indicators <-
     data %>%
     ungroup() %>%
     filter(country_name == base_country) %>%
-    select(where(is.na))
+    select(where(is.na)) %>%
+    names
 
-  na_indicators <-
-    if(length(na_indicators)==0) {
-      NULL
-    } else {
-      na_indicators <- na_indicators %>% pivot_longer(cols = 1:ncol(.), names_to = "missing_var")
-    }
+# List final relevant variables: those selected, minus those missing
+  variables <-
+    setdiff(vars, na_indicators)
 
+  variables <-
+    intersect(variables, names(data))
+
+# This is the relevant data to be used
   quantiles <-
     data %>%
     ungroup() %>%
+
+    # Keep only the base and comparison countries
     filter(
-      country_name %in% comparison_list$country_name | country_name == base_country
+      (country_name %in% comparison_list$country_name) | (country_name == base_country)
     ) %>%
+
+    # Keep only selected, non-missing indicators
     select(
-      country_name, all_of(vars)
+      country_name,
+      all_of(variables)
     )
 
-  quantiles_group <-
-    comparison_list %>%
-    select(country_name) %>%
-    right_join(quantiles, by = c("country_name")) %>%
-    pivot_longer(cols = all_of(vars), names_to = "variable") %>%
-    filter(! variable %in% na_indicators$missing_var) %>%
-    left_join(variable_names,
-              by = "variable") %>%
-    filter(!is.na(value)) %>%
-    group_by(
-      country_name, variable, var_name
+# Merge with variable dictionary
+  quantiles <-
+    quantiles %>%
+
+    # Make long per indicator
+    pivot_longer(
+      cols = all_of(variables),
+      names_to = "variable"
     ) %>%
-    summarise(dtf = mean(value)) %>%
+
+    # Add variables definition and family
+    left_join(
+      variable_names,
+      by = "variable"
+    )
+
+# Calculate quantiles
+  quantiles <-
+    quantiles %>%
+
+    # Remove missing values
+    filter(!is.na(value)) %>%
+
+    # Calculate relevant indicators
     group_by(variable, var_name) %>%
     mutate(
-      dtt = percent_rank(dtf),
-      q25 = quantile(dtf, c(0.25)),
-      q50 = quantile(dtf, c(0.5)),
+      dtt = percent_rank(value),
+      q25 = quantile(value, c(0.25)),
+      q50 = quantile(value, c(0.5)),
       status = case_when(
         dtt <= .25 ~ "Weak\n(bottom 25%)",
         dtt > .25 & dtt <= .50 ~ "Emerging\n(25% - 50%)",
         dtt > .50 ~ "Strong\n(top 50%)"
       )
     ) %>%
-    ungroup
+    ungroup %>%
+    rename(dtf = value)
 
-  base_low_var <-
-    quantiles_group %>%
+# Remove indicators where there is too little variance
+  low_variance_indicators <-
+    quantiles %>%
     filter(country_name == base_country & q25==q50) %>%
-    .$variable
+    select(variable) %>%
+    unlist
 
-  quantiles_group <-
-    quantiles_group %>%
-    filter(! variable %in% base_low_var)
-
-  return(quantiles_group)
+  quantiles <-
+    quantiles %>%
+    filter(!(variable %in% low_variance_indicators))
 
 }
