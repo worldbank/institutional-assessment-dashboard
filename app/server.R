2d271e35cb0a5f10af484d50d673f1991c0bc959
Luiza <luiza.c.andrade@gmail.com>
2022-05-23 15:28:42 -0400

    #110

--- app/server.R
+++ app/server.R
@@ -97,38 +97,6 @@
        ignoreNULL = FALSE
     )
 
-
-    observeEvent(
-      input$select,
-
-      {
-        toggleState(
-          id = "report",
-          condition = input$select
-        )
-
-        updatePickerInput(
-          session,
-          "country_bar",
-          selected = input$country
-        )
-
-        updatePickerInput(
-          session,
-          "countries_bar",
-          selected = input$countries
-        )
-
-        updatePickerInput(
-          session,
-          "groups_bar",
-          selected = input$groups
-        )
-      },
-
-      ignoreNULL = FALSE
-    )
-
     # Reactive objects ==============================================
 
     ## Benchmark data -----------------------------------------------
@@ -217,7 +185,190 @@
 
         }
       )
-    
+
+    ## Update inputs based on benchmark selection ------------------------------
+    observeEvent(
+      input$select,
+
+      {
+        # Create report
+        toggleState(
+          id = "report",
+          condition = input$select
+        )
+
+        # Cross-crountry comparison selection
+        updatePickerInput(
+          session,
+          "country_bar",
+          selected = input$country
+        )
+
+        updatePickerInput(
+          session,
+          "countries_bar",
+          selected = input$countries
+        )
+
+        updatePickerInput(
+          session,
+          "groups_bar",
+          selected = input$groups
+        )
+
+        # Bivariate correlation selection
+        updatePickerInput(
+          session,
+          "country_scatter",
+          selected = input$country
+        )
+
+        updatePickerInput(
+          session,
+          "countries_scatter",
+          selected = input$countries
+        )
+
+        updatePickerInput(
+          session,
+          "high_group",
+          selected = input$groups
+        )
+
+        # Time trends
+        updatePickerInput(
+          session,
+          "country_trends",
+          selected = input$country
+        )
+
+        updatePickerInput(
+          session,
+          "countries_trends",
+          selected = input$countries
+        )
+
+        updatePickerInput(
+          session,
+          "group_trends",
+          selected = input$groups
+        )
+
+      },
+
+      ignoreNULL = FALSE
+    )
+
+    ## Change variable selection in all tabs
+
+    observeEvent(
+      input$vars_bar,
+
+      {
+        updatePickerInput(
+          session,
+          "y_scatter",
+          selected = input$vars_bar
+        )
+
+        updatePickerInput(
+          session,
+          "vars_map",
+          selected = input$vars_bar
+        )
+
+        updatePickerInput(
+          session,
+          "vars_trends",
+          selected = input$vars_bar
+        )
+
+      },
+
+      ignoreNULL = FALSE
+    )
+
+    observeEvent(
+      input$y_scatter,
+
+      {
+        updatePickerInput(
+          session,
+          "vars_bar",
+          selected = input$y_scatter
+        )
+
+        updatePickerInput(
+          session,
+          "vars_map",
+          selected = input$y_scatter
+        )
+
+        updatePickerInput(
+          session,
+          "vars_trends",
+          selected = input$y_scatter
+        )
+
+      },
+
+      ignoreNULL = FALSE
+    )
+
+    observeEvent(
+      input$vars_map,
+
+      {
+        updatePickerInput(
+          session,
+          "vars_bar",
+          selected = input$vars_map
+        )
+
+        updatePickerInput(
+          session,
+          "y_scatter",
+          selected = input$vars_map
+        )
+
+        updatePickerInput(
+          session,
+          "vars_trends",
+          selected = input$vars_map
+        )
+
+      },
+
+      ignoreNULL = FALSE
+    )
+
+    observeEvent(
+      input$vars_trends,
+
+      {
+        updatePickerInput(
+          session,
+          "vars_bar",
+          selected = input$vars_trends
+        )
+
+        updatePickerInput(
+          session,
+          "y_scatter",
+          selected = input$vars_trends
+        )
+
+        updatePickerInput(
+          session,
+          "vars_map",
+          selected = input$vars_trends
+        )
+
+      },
+
+      ignoreNULL = FALSE
+    )
+
     ## Make sure only valid groups are chosen ----------------------------------
 
     observeEvent(
@@ -408,7 +559,7 @@
         static_scatter(
           global_data,
           input$country_scatter,
-          input$countries,
+          input$countries_scatter,
           high_group(),
           input$y_scatter,
           input$x_scatter,
@@ -453,7 +604,7 @@
 
     var_trends <-
       eventReactive(
-        input$indicator_trends,
+        input$vars_trends,
 
         {
           var_selected <-
@@ -463,38 +614,40 @@
         }
       )
 
-    observeEvent(
-      input$indicator_trends,
+    eventReactive(
+      input$vars_trends,
 
       {
-        valid_countries <-
-          raw_data %>%
-          filter(!is.na(get(var_trends()))) %>%
-          select(country_name) %>%
-          unique %>%
-          arrange(country_name) %>%
-          unlist %>%
-          unname
+        if (!is.null(input$vars_trends)) {
+          valid_countries <-
+            raw_data %>%
+            filter(!is.na(get(var_trends()))) %>%
+            select(country_name) %>%
+            unique %>%
+            arrange(country_name) %>%
+            unlist %>%
+            unname
 
-        last_country <-
-          input$country_trends
+          last_country <-
+            input$country_trends
 
-        updatePickerInput(
-          session,
-          "country_trends",
-          choices = valid_countries,
-          selected = last_country
-        )
+          updatePickerInput(
+            session,
+            "country_trends",
+            choices = valid_countries,
+            selected = last_country
+          )
 
-        last_countries <-
-          input$countries_trends
+          last_countries <-
+            input$countries_trends
 
-        updatePickerInput(
-          session,
-          "countries_trends",
-          choices = valid_countries,
-          selected = last_countries
-        )
+          updatePickerInput(
+            session,
+            "countries_trends",
+            choices = valid_countries,
+            selected = last_countries
+          )
+        }
       },
 
       ignoreNULL = FALSE
@@ -503,12 +656,12 @@
     output$time_series <-
       renderPlotly({
 
-        if (input$indicator_trends != "") {
+        if (input$vars_trends != "") {
 
           trends_plot(
             raw_data,
             var_trends(),
-            input$indicator_trends,
+            input$vars_trends,
             input$country_trends,
             input$countries_trends,
             country_list,
--- app/ui.R
+++ app/ui.R
@@ -290,7 +290,7 @@ ui <-
                 pickerInput(
                   "vars_bar",
                   label = "Select indicator",
-                  choices = c("", variable_list),
+                  choices = variable_list,
                   selected = NULL,
                   options = list(
                     size = 20,
@@ -372,8 +372,8 @@ ui <-
                 pickerInput(
                   "country_scatter",
                   label = "Select a base country",
-                  choices = countries,
-                  selected = "Uruguay",
+                  choices = c("", countries),
+                  selected = NULL,
                   multiple = FALSE,
                   options = list(
                     size = 20,
@@ -391,7 +391,7 @@ ui <-
                     "Log GDP per capita, PPP",
                     variable_list
                   ),
-                  selected = "Central bank independence",
+                  selected = NULL,
                   options = list(
                     `live-search` = TRUE,
                     size = 20,
@@ -444,13 +444,35 @@ ui <-
           ),
 
           bs4Card(
+            title = "Select individual comparison countries",
             width = 11,
-            solidHeader = FALSE,
-            gradientColor = "primary",
-            collapsible = FALSE,
-            plotlyOutput(
-              "scatter_plot",
-              height = paste0(plot_height, "px")
+            status = "success",
+            collapsed = TRUE,
+
+            checkboxGroupButtons(
+              inputId = "countries_scatter",
+              individual = TRUE,
+              label = NULL,
+              choices = countries,
+              checkIcon = list(
+                yes = icon("ok",
+                           lib = "glyphicon")
+              )
+            )
+          ),
+
+          conditionalPanel(
+            'input.y_scatter !== ""',
+
+            bs4Card(
+              width = 11,
+              solidHeader = FALSE,
+              gradientColor = "primary",
+              collapsible = FALSE,
+              plotlyOutput(
+                "scatter_plot",
+                height = paste0(plot_height, "px")
+              )
             )
           )
         ),
@@ -472,10 +494,10 @@ ui <-
               column(
                 width = 3,
                 pickerInput(
-                  "indicator_trends",
+                  "vars_trends",
                   label = "Select indicator to visualize",
                   choices = variable_list,
-                  selected = "Capital controls",
+                  selected = NULL,
                   options = list(
                     `live-search` = TRUE,
                     size = 21,
@@ -490,8 +512,8 @@ ui <-
                 pickerInput(
                   "country_trends",
                   label = "Select a base country",
-                  choices = countries,
-                  selected = "Uruguay",
+                  choices = c("", countries),
+                  selected = NULL,
                   multiple = FALSE,
                   options = list(
                     size = 23
@@ -500,12 +522,12 @@ ui <-
               ),
 
               column(
-                width = 3,
+                width = 6,
                 pickerInput(
                   "group_trends",
                   label = "Select comparison groups",
                   choices = group_list,
-                  selected = c("OECD members", "Latin America & Caribbean"),
+                  selected = NULL,
                   multiple = TRUE,
                   options = list(
                     maxOptions = 5,
@@ -513,33 +535,36 @@ ui <-
                     size = 21
                   )
                 )
-              ),
-
-              column(
-                width = 3,
-                pickerInput(
-                  "countries_trends",
-                  label = "Select comparison countries",
-                  choices = countries,
-                  multiple = TRUE,
-                  options = list(
-                    `actions-box` = TRUE,
-                    size = 22,
-                    title = "Click to select family or indicator"
-                  )
-                )
               )
             )
           ),
 
           bs4Card(
+            title = "Select individual comparison countries",
             width = 11,
-            solidHeader = FALSE,
-            gradientColor = "primary",
-            collapsible = FALSE,
+            status = "success",
+            collapsed = TRUE,
 
-            conditionalPanel(
-              "input.indicator_trends !== ''",
+            checkboxGroupButtons(
+              inputId = "countries_trends",
+              individual = TRUE,
+              label = NULL,
+              choices = countries,
+              checkIcon = list(
+                yes = icon("ok",
+                           lib = "glyphicon")
+              )
+            )
+          ),
+
+          conditionalPanel(
+            'input.vars_trends !== null && input.country_trends != ""',
+
+            bs4Card(
+              width = 11,
+              solidHeader = FALSE,
+              gradientColor = "primary",
+              collapsible = FALSE,
               plotlyOutput(
                 "time_series",
                 height = paste0(plot_height, "px")
@@ -568,7 +593,7 @@ ui <-
                   "vars_map",
                   label = "Select indicator",
                   choices = variable_list,
-                  selected = "Capital controls",
+                  selected = NULL,
                   options = list(
                     `live-search` = TRUE,
                     size = 20,
@@ -592,14 +617,14 @@ ui <-
             )
           ),
 
-          bs4Card(
-            width = 11,
-            solidHeader = FALSE,
-            gradientColor = "primary",
-            collapsible = FALSE,
+          conditionalPanel(
+            "input.vars_map !== ''",
 
-            conditionalPanel(
-              "input.vars_map !== ''",
+            bs4Card(
+              width = 11,
+              solidHeader = FALSE,
+              gradientColor = "primary",
+              collapsible = FALSE,
               plotlyOutput(
                 "map",
                 height = paste0(plot_height, "px")
