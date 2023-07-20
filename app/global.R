655aff4894276b399e75e45a30efc834c0ceb53f
Shelmith Kariuki <kariukishelmith@gmail.com>
2023-07-20 17:40:39 +0300

    Changed the way packages are loaded in the global.R file. The new method results into the packages being automatically installed incase they are missing.

--- app/global.R
+++ app/global.R
@@ -1,27 +1,27 @@
-# Packages ----------------------------------------------------------------
-library(tidyverse)
-library(DT)
-library(plotly)
-library(shiny)
-library(shinyjs)
-library(shinyBS)
-library(shinycssloaders)
-library(shinybusy)
-library(shinyWidgets)
-library(shinyhelper)
-library(bs4Dash)
-library(fresh)
-library(sf)
-library(haven)
-library(zoo)
-library(formattable)
-library(here)
-library(data.table)
-library(hrbrthemes)
-library(bsplus)
-library(htmltools)
-library(officer)
-library(rvg)
+## Install and load all the packages that will be used in the app ----------------------------------------------------------------
+
+## The steps below ensure that missing packages are automatically installed and loaded 
+
+### i) Create a vector of all the packages used in this project
+pkgs <- c("tidyverse", "DT", "plotly", "shiny", "shinyjs", "shinyBS",
+          "shinycssloaders", "shinybusy", "shinyWidgets", "shinyhelper", 
+          "bs4Dash", "fresh", "sf", "haven", "zoo", "formattable", "here",
+          "data.table", "hrbrthemes", "bsplus", "htmltools", "officer", "rvg")
+
+### ii) Extract the missing packages
+miss_pkgs <- pkgs[!pkgs %in% installed.packages()[,1]] 
+
+### iii) Install them
+if(length(miss_pkgs)>0){
+  install.packages(miss_pkgs)
+}
+
+### iv) Load the packages
+invisible(lapply(pkgs,library,character.only=TRUE))
+
+### v) Clear memory by removing objects that are no longer needed.
+rm(miss_pkgs, pkgs)
+
 
 ## Auxiliary functions -----------------------------------------------------------------
 
@@ -48,6 +48,12 @@ source(here("auxiliary", "fun_low_variance.R"))
 
 # Create benchmark graphs
 source(here("auxiliary", "plots.R"))
+
+# Function that displays publications
+source(here("auxiliary", "fun_publications.R"))
+
+# Modules
+source(here("modules", "mod_publications.R"))
 
 # Data -------------------------------------------------------------
 
@@ -111,13 +117,13 @@ country_list <-
 
 
 
-spatial_data <-
-  read_rds(
-    here(
-      "data",
-      "indicators_map.rds"
-    )
-  )
+# spatial_data <-
+#   read_rds(
+#     here(
+#       "data",
+#       "indicators_map.rds"
+#     )
+#   )
 
 
 
@@ -135,7 +141,7 @@ for(i in 1:nrow(clean_country)){
     ctf_long$country_name[ctf_long$country_name==clean_country[i,'Country']]=clean_country[i,'Clean_Names']
     raw_data$country_name[raw_data$country_name==clean_country[i,'Country']]=clean_country[i,'Clean_Names']
     global_data$country_name[global_data$country_name==clean_country[i,'Country']]=clean_country[i,'Clean_Names']
-    spatial_data$country_name[spatial_data$country_name==clean_country[i,'Country']]=clean_country[i,'Clean_Names']
+    # spatial_data$country_name[spatial_data$country_name==clean_country[i,'Country']]=clean_country[i,'Clean_Names']
  }
 } 
 
@@ -144,10 +150,10 @@ country_list = country_list[order(country_list$country_name, decreasing = FALSE)
 ctf_long = ctf_long[order(ctf_long$country_name, decreasing = FALSE), ]
 raw_data = raw_data[order(raw_data$country_name, decreasing = FALSE), ]
 global_data = global_data[order(global_data$country_name, decreasing = FALSE), ]
-spatial_data = spatial_data[order(spatial_data$country_name, decreasing = FALSE), ]
+# spatial_data = spatial_data[order(spatial_data$country_name, decreasing = FALSE), ]
 
 
-st_crs(spatial_data) <- "+proj=robin"
+# st_crs(spatial_data) <- "+proj=robin"
 
 # Load data control
 db_variables <-
