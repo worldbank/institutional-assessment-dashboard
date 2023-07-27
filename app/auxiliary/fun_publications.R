12f3a158835fbc2ba6e98b8104cef333147aa374
Shelmith Kariuki <kariukishelmith@gmail.com>
2023-07-27 16:21:51 +0300

    Refined the Publications tab. We are now reading the data arguments from an excel sheet that I'll upload to OneDrive later. I'll also comment the code later

--- app/auxiliary/fun_publications.R
+++ app/auxiliary/fun_publications.R
@@ -13,27 +13,33 @@
 #' @export
 #'
 #' @examples
+
+pubList <- readxl::read_excel("www/publications/publicationsList.xlsx")
+
 pub_function <- function(image,
                          title,
                          country,
                          year,
-                         authors,
-                         downloadID) {
+                         authors) {
+  
+  
+  if(country %in% pubList$country[pubList$title == title]){
+
   shiny::div(
     style = 
-    "height: 100%;
+     "height: 100%;
     /* grid-column-gap: 1.5625vw; */
     grid-row-gap: 1.5625vw;
     grid-template-rows: auto;
     grid-template-columns: 25% 75%;
     grid-auto-columns: 1fr;
     display: grid;
-    border-top: 4px solid #051f3f;
+    border: 4px solid #051f3f;
     /*width: 352px; */
     ",
     shiny::div(
       style = 
-      "background-color: #d8dcdb;
+        "background-color: #d8dcdb;
        justify-content: center;
         align-items: center;
         display: flex;
@@ -45,7 +51,7 @@ pub_function <- function(image,
     ),
     shiny::div(
       style = 
-      "display: flex;
+        "display: flex;
        /*height: 250px;*/
       flex-direction: column;
       justify-content: space-evenly;
@@ -55,35 +61,35 @@ pub_function <- function(image,
       border: 0 solid rgba(0,0,0,0.125);
       background: white;
       /*margin: 30px 0 0 0;*/
-      padding: 15px;
-      text-align: center;
+      padding: 5px 15px;
+      text-align: left;
       color: #051f3f;'
       ",
       shiny::span(paste0(title),
-        style = "font-size:20px; font-weight:bold; padding-top: 15px;"
+                  style = "font-size:14px; font-weight:bold; padding: 5px 0;"
       ),
-      shiny::br(),
       shiny::span(paste0(country),
-        style = "font-size:18px; font-weight:bold;"
+                  style = "font-size:12px; font-weight:bold; padding: 5px 0;"
       ),
-      shiny::br(),
       shiny::span(paste0(year),
-        style = "font-size:16px;"
+                  style = "font-size:10px; padding: 5px 0;"
       ),
-      shiny::br(),
       shiny::span(paste0(authors),
-        style = "font-size:16px; font-style:italic;"
-      ),
-      shiny::br(),
-      shiny::downloadButton(
-        outputId = downloadID,
-        "Download",
-        style = "color: white; background-color: #33a2b7;
-                                          text-align: center;"
+                  style = "font-size:10px; font-style:italic; padding: 5px 0;"
       )
     )
   )
+}else{
+  shiny::HTML(paste0("No report found for ", country))
 }
+}
+
+# pub_function(image = "publications/CEM_Croatia.png",
+#              title =  "Climate Change Institutional Assessment (CCIA)",
+#              country = "Croatia",
+#              year = "2022",
+#             authors = "World Bank")
+  
 
 #' pub_function_download
 #'
@@ -95,11 +101,11 @@ pub_function <- function(image,
 #' @export
 #'
 #' @examples
-pub_function_download <- function(filename) {
-  shiny::downloadHandler(
-    filename = filename,
-    content = function(file) {
-      file.copy(paste0("www/publications/", filename), file)
-    }
-  )
-}
+# pub_function_download <- function(filename) {
+#   shiny::downloadHandler(
+#     filename = filename,
+#     content = function(file) {
+#       file.copy(paste0("www/publications/", filename), file)
+#     }
+#   )
+# }
--- app/modules/mod_publications.R
+++ app/modules/mod_publications.R
@@ -11,54 +11,155 @@
 publicationsUI <- function(id) {
   ns <- NS(id)
   tagList(
+    shinyWidgets::useBs4Dash(),
 
-    ## Each row will contain 3 publications, each with a width of 4. A whole row has a width of 12
-    shiny::fluidRow(
-      shiny::column(
-        width = 4,
-        ## pub_function() is documented in the fun_publications.R file
-        ## contained in the auxiliary folder
-        pub_function(
-          image = "publications/CEM_Croatia.png",
-          title = "Country Economic Memorandum (CEM)",
-          country = "Croatia",
-          year = "2022",
-          authors = "Author 1: Jane Doe",
-          downloadID = ns("download_pubs_1")
-        )
-      ),
-      shiny::column(
-        width = 4,
-        pub_function(
-          image = "publications/CCIA_Mozambique.png",
-          title = "Climate Change Institutional Assessment (CCIA)",
-          country = "Mozambique",
-          year = "2022",
-          authors = "Author 1: John Doe",
-          downloadID = ns("download_pubs_2")
+    ## 
+    shiny::fluidRow(style = "height:15px"),
+    
+    ## Country
+    column(
+      width = 3,
+      pickerInput(
+        ns("country"),
+        label = "Select a country",
+        choices = c("All", countries),
+        selected = "All",
+        multiple = FALSE,
+        options = list(
+          size = 20,
+          `actions-box` = TRUE
         )
       )
+    ),
+    
+    shiny::fluidRow(style = "height:15px"),
+    
+    ## Each row will contain 3 publications, each with a width of 4. A whole row has a width of 12
+        uiOutput(ns('publications'))
     )
-  )
+  
 }
 
 publicationsServer <- function(id) {
   moduleServer(
     id,
     function(input, output, session) {
-      # Publication downloads
-      output$download_pubs_1 <- pub_function_download("Croatia CEM.pdf")
-      output$download_pubs_2 <- pub_function_download("Mozambique CCIA  -  Mainstreaming Climate Change in Governance - P172569_1.pdf")
-    }
-  )
+     
+      sel_country <- reactive({
+        input$country
+      })
+      
+      
+      ## pub_function() is documented in the fun_publications.R file
+      ## contained in the auxiliary folder
+    
+      
+      output$publications <- renderUI({
+        print(sel_country())
+        ## Each card will be a publications title
+        n_cards <- length(unique(pubList$title))
+        ui_cards <- c()
+        
+        lapply(1:n_cards, function(i) {
+
+          ## Each card will hold publications for the different countries, all in one
+          ## row, that will be scrollable
+          
+          ui_parts_rows <- c()
+          ui_parts_cols <- c()
+          
+          ui_tot_pubs_per_card <- length(pubList$title[pubList$title == unique(pubList$title)[i]])
+          ui_cols <- ui_tot_pubs_per_card
+          ui_rows <- 1
+          
+          pubList_per_card <- pubList %>% dplyr::filter(title %in% unique(pubList$title)[i])
+          
+          ui_cards[[i]] <- bs4Dash::bs4Card(
+              title = unique(pubList$title)[i],
+              solidHeader = TRUE,
+              width = 12,
+              status = "navy",
+              collapsible = TRUE,
+              inputId = paste0("pub_card", i),
+                
+              ## Country selector
+              shiny::fluidRow(),
+              
+              ## Publications
+                lapply(1:ui_rows, function(i) {
+                  
+                  ui_parts_rows[[i]] <-
+                    
+                    shiny::fluidRow(
+                      style = "display: flex;
+                       margin-top: 15px;
+                       margin-bottom: 30px;
+                       justify-content: space-between;
+                     ",
+                      
+                      if(sel_country() == "All"){
+                        
+                      
+                      lapply(1:ui_cols, function(j) {
+                        
+                        ui_parts_cols[[j]] <-
+                          
+                          shiny::column(
+                            width = 3,
+                              pub_function(image = pubList_per_card$image[j],
+                                           title = pubList_per_card$title[j],
+                                           country = pubList_per_card$country[j],
+                                           year = pubList_per_card$year[j],
+                                           authors = pubList_per_card$authors[j]
+                              )
+                          )
+                            }
+                          )
+                      }else{
+                        shiny::column(
+                          width = 3,
+                          if(sel_country() %in% pubList_per_card$country){
+                            pub_function(image = pubList_per_card$image[pubList_per_card$country == sel_country()],
+                                         title = pubList_per_card$title[pubList_per_card$country == sel_country()],
+                                         country = pubList_per_card$country[pubList_per_card$country == sel_country()],
+                                         year = pubList_per_card$year[pubList_per_card$country == sel_country()],
+                                         authors = pubList_per_card$authors[pubList_per_card$country == sel_country()]
+                            )
+                          }else{
+                            shiny::HTML(paste0("No report found for ", sel_country()))
+                          }
+                            
+                         
+                        )
+                      }
+                      
+                    )
+                        
+                        
+                      })
+                    )
+                  
+                  
+                  
+                })
+          
+                
+              })
+
+        })
+
 }
 
-# ui <- fluidPage(
-#   publicationsUI("publications")
-# )
-#
-# server <- function(input, output, session) {
-#   publicationsServer("publications")
-# }
-#
-# shinyApp(ui, server)
+
+
+library(shiny)
+
+ui <- fluidPage(
+  publicationsUI("sth") 
+)
+
+server <- function(input, output, session) {
+  publicationsServer("sth")
+}
+
+shinyApp(ui, server)
