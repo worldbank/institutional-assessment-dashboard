83dc93e38f648793e963ede1483fbf591952f0cd
Shelmith Kariuki <kariukishelmith@gmail.com>
2023-08-07 23:18:53 +0300

    Created dynamic input fields for custom names. Input fields can now be added without refreshing everything

--- app/modules/mod_controls.R
+++ app/modules/mod_controls.R
@@ -31,7 +31,10 @@ controlsUI <- function(id) {
     shiny::fluidRow(
       shiny::column(
         width = 3,
-        prettyCheckbox(
+      shiny::column(
+        width = 6,
+        align = "left",
+        shinyWidgets::prettyCheckbox(
           inputId = ns("create_custom_grps"),
           label = "Create custom groups",
           value = FALSE,
@@ -39,6 +42,12 @@ controlsUI <- function(id) {
           status = "success"
         )
       ),
+      shiny::column(
+        width = 6,
+        align = "right",
+        shiny::actionButton(ns("reset_btn"), label = "", icon = icon("info"))
+      )
+    ),
       shiny::column(
         width = 3,
         shiny::numericInput(
@@ -73,8 +82,19 @@ controlsServer <- function(id) {
       ## Hide custom group fields until one chooses to create them ---------------
       shinyjs::hide("custom_grps_count")
       shinyjs::hide("custom_grps")
-      shinyjs::hide("custom_groups_table")
+      shinyjs::hide("reset_btn")
+      # shinyjs::hide("custom_groups_table")
 
+
+      ## Object that holds reactives
+      rv <- shiny::reactiveValues(
+        
+        custom_group_fields_reactive = NULL,
+        custom_grps_count = NULL
+      )
+      
+      
+      
       ## Update labels and of comp_cat_choices ------------------------
       shiny::observeEvent(input$comp_cat, {
         comp_cat <- input$comp_cat
@@ -86,114 +106,131 @@ controlsServer <- function(id) {
         )
       })
 
-      
-      
-
-      ## When one chooses to create custom groups, show the number of groups field
-      shiny::observeEvent(input$create_custom_grps, {
-        if (input$create_custom_grps == T) {
-          shinyjs::show("custom_grps_count")
-        } else {
-          shinyjs::hide("custom_grps_count")
-        }
-      })
-
-
       ## Show the custom groups ----------------------------
 
       shiny::observeEvent(input$create_custom_grps, {
-        if (input$create_custom_grps == T & input$custom_grps_count > 0) {
+        
+        if (input$create_custom_grps == T) {
+          shinyjs::show("custom_grps_count")
+          # shinyjs::reset("custom_grps_count")
           shinyjs::show("custom_grps")
+          shinyjs::reset("custom_grps")
+          shinyjs::show("reset_btn")
         } else {
+          shinyjs::hide("custom_grps_count")
           shinyjs::hide("custom_grps")
+          shinyjs::reset("custom_grps")
+          shinyjs::hide("reset_btn")
         }
       })
+      
 
+
+      
       ## Create the custom groups ----------------------------
 
-      shiny::observeEvent(input$create_custom_grps, {
-        output$custom_grps <- renderUI({
+      custom_group_fields_reactive <- shiny::reactive({
+        
+        # shiny::req(input$custom_grps_count)
+        
+        ## number of fields to create
+        n_fields <- input$custom_grps_count
+        
+        
+        ## create an object that will hold each custom group field (name and country drop-downs)
+        ui_fields <- c()
+        
+        ## for each custom group ....
+        lapply(1:n_fields, function(i) {
           
-          ## number of fields to create
-          n_fields <- input$custom_grps_count
+          custom_names <- ""
+          custom_countries <- NULL
+          
+          if(n_fields >= 1){
+
+            custom_names <- isolate(input[[paste("custom_grps_names", i, sep = "_")]])
+            custom_countries <- isolate(input[[paste("custom_grps_countries", i, sep = "_")]])
 
 
-          ## create an object that will hold each custom group field (name and country drop-downs)
-          ui_fields <- c()
-
-
-          ## for each custom group ....
-          lapply(1:n_fields, function(i) {
-            ui_fields[[i]] <- shiny::fluidRow(
-              shiny::column(
-                width = 6,
-                shiny::textInput(
-                  inputId = ns(paste("custom_grps_names", i, sep = "_")),
-                  label = paste("Insert the name of group ", i)
-                )
-              ),
-              shiny::column(
-                width = 6,
-                pickerInput(
-                  inputId = ns(paste("custom_grps_countries", i, sep = "_")),
-                  label = paste("Select countries that fall into group ", i),
-                  choices = c("", countries),
-                  selected = NULL,
-                  multiple = TRUE,
-                  options = list(
-                    `actions-box` = TRUE,
-                    `live-search` = TRUE
-                  )
+          ui_fields[[i]] <- shiny::fluidRow(
+            shiny::column(
+              width = 6,
+              shiny::textInput(
+                inputId = ns(paste("custom_grps_names", i, sep = "_")),
+                label = paste("Insert the name of group ", i),
+                value = custom_names
+              )
+            ),
+            shiny::column(
+              width = 6,
+              shinyWidgets::pickerInput(
+                inputId = ns(paste("custom_grps_countries", i, sep = "_")),
+                label = paste("Select countries that fall into group ", i),
+                choices = c("", countries),
+                selected = custom_countries,
+                multiple = TRUE,
+                options = list(
+                  `actions-box` = TRUE,
+                  `live-search` = TRUE
                 )
               )
             )
-          })
-        })
+          )
+          }
+      })
       })
 
-      ## table
       
-      shiny::observeEvent(input$submit_btn, {
+      shiny::observeEvent(input$create_custom_grps, {
         
-        if (input$create_custom_grps == T & input$custom_grps_count > 0) {
-          shinyjs::show("custom_groups_table")
-        } else {
-          shinyjs::hide("custom_groups_table")
-        }
+        output$custom_grps <- renderUI({
+          
+          custom_group_fields_reactive()
+          
+        })
+      })
+      
+      # shiny::observeEvent(input$custom_grps_count, {
+      #   
+      # rv$custom_grps_count <-  input$custom_grps_count
+      # })
+      # 
+      # 
+      # shiny::observeEvent(input$reset_btn, {
+      #   
+      #   for(i in 1: rv$custom_grps_count){
+      #     
+      #     # shiny::updateTextInput(
+      #     #   session = session,
+      #     #   inputId = paste("custom_grps_names", i, sep = "_"),
+      #     #   value = ""
+      #     # )
+      #     
+      #     shinyWidgets::updatePickerInput(
+      #       session = session,
+      #       inputId = paste("custom_grps_countries", i, sep = "_"),
+      #       selected = NULL
+      #     ) 
+      # 
+      #   }
+      # })
+
+      ## table
+
 
 
         predetermined_groups_table_reactive <- shiny::eventReactive(input$submit_btn, {
-          
-          # shiny::req(input$comp_cat)
-          # shiny::req(input$comp_cat_choices)
-          
           comp_cat <- input$comp_cat
           comp_cat_choices <- input$comp_cat_choices
-          
-          if(!is.null(comp_cat) & !is.null(comp_cat_choices)){
-            
-          checker = apply(CLASS_new, 2, function(x) any(comp_cat_choices %in% x))
-          var = names(checker[checker == TRUE])
-          var_sym = sym(var)
-          
-          country_selection <- CLASS_new %>% 
-                                 select(Economy, {{var_sym}}) %>% 
-                                 filter({{var_sym}} %in% comp_cat_choices) %>% 
-                                 mutate(Category = var) %>% 
-                                 rename(Grp = {{var_sym}}, Countries = Economy) %>% 
-                                 select(Category, Grp, Countries)
-          }else{
-           country_selection <- NULL 
-          }
-          
-          
 
+          predetermined_groups_table_func(comp_cat, comp_cat_choices)
         })
-                
+
         custom_groups_table_reactive <- shiny::eventReactive(input$submit_btn, {
           
-          # shiny::req(input$custom_grps_names_1)
-          # shiny::req(input$custom_grps_countries_1)
+          
+          if(input$create_custom_grps &
+              !is.null(input$custom_grps_names_1) & !is.null(input$custom_grps_countries_1)){
           
           n_fields <- input$custom_grps_count
 
@@ -202,30 +239,40 @@ controlsServer <- function(id) {
           for (i in 1:n_fields) {
             grp_name <- as.character(input[[paste("custom_grps_names", i, sep = "_")]])
             country_selection <- as.vector(input[[paste("custom_grps_countries", i, sep = "_")]])
-            custom_grps_list[[i]] <- func_custom_grp(category = "Custom", name = grp_name, countriez = country_selection)
+            custom_grps_list[[i]] <- data.frame(Category = "Custom", Grp = grp_name, Countries = country_selection)
           }
 
-          if(length(custom_grps_list) > 0){
-          custom_grps_df <- bind_rows(custom_grps_list)
-          }else{
+          if (length(custom_grps_list) > 0) {
+            custom_grps_df <- dplyr::bind_rows(custom_grps_list)
+          } else {
             custom_grps_df <- NULL
           }
+          }else{
+            custom_grps_df <- NULL  
+          }
           return(custom_grps_df)
-
         })
-        
-        output$custom_groups_table <- shiny::renderUI({
-        
-        if(!is.null(custom_groups_table_reactive()) > 0){
+
+        shiny::observeEvent(input$submit_btn, {
           
-          dat <- bind_rows(predetermined_groups_table_reactive(), 
-                          custom_groups_table_reactive()
-                            )
+          
+          output$custom_groups_table <- shiny::renderUI({
+          
+            print(predetermined_groups_table_reactive())
+            print(custom_groups_table_reactive())
+            
+        if (!is.null(predetermined_groups_table_reactive())|!is.null(custom_groups_table_reactive())) {
+          dat <- dplyr::bind_rows(
+            predetermined_groups_table_reactive(),
+            custom_groups_table_reactive()
+          )
           
           reactable::reactable(data = dat)
+        } else {
+          NULL
         }
-          
         })
+
       })
     }
   )
