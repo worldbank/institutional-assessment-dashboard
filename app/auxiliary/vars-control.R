fd46224c3a52d0c108b9752e1054e69efc1ca4dc
Galileu Kim <galileukim@worldbank.org>
2023-09-08 10:24:51 -0400

    fix: finalize changes to data ETL

--- app/auxiliary/vars-control.R
+++ app/auxiliary/vars-control.R
@@ -1,13 +1,21 @@
-# Anti-Corruption, Transparency and Accountability institutions ======================
-vars_transp <-
+db_variables <-
+  read_rds(
+    here(
+      "data",
+      "db_variables.rds"
+    )
+  )
+
+# 1. Anti-corruption institutions ======================
+vars_anticorruption <-
   db_variables %>%
   filter(
     var_level == "indicator",
-    family_var == "vars_transp"
+    family_var == "vars_anticorruption"
   ) %>%
   pull(variable)
 
-# Business environment and trade institutions ======================
+# 2. Business environment and trade institutions ======================
 vars_mkt <-
   db_variables %>%
   filter(
@@ -16,7 +24,25 @@ vars_mkt <-
   ) %>%
   pull(variable)
 
-# Financial market institutions ======================
+# 3. Climate Change and Environment Institutions ======================
+vars_climate <-
+  db_variables %>%
+  filter(
+    var_level == "indicator",
+    family_var == "vars_climate"
+  ) %>%
+  pull(variable)
+
+# 4. Digital and Data Institutions ======================
+vars_digital <-
+  db_variables %>%
+  filter(
+    var_level == "indicator",
+    family_var == "vars_digital"
+  ) %>%
+  pull(variable)
+
+# 5. Financial market institutions ======================
 vars_fin <-
   db_variables %>%
   filter(
@@ -25,7 +51,7 @@ vars_fin <-
   ) %>%
   pull(variable)
 
-# Labor market institutions ======================
+# 6. Labor market institutions ======================
 vars_lab <-
   db_variables %>%
   filter(
@@ -34,7 +60,7 @@ vars_lab <-
   ) %>%
   pull(variable)
 
-# Legal institutions ======================
+# 7. Justice institutions ======================
 vars_leg <-
   db_variables %>%
   filter(
@@ -43,7 +69,7 @@ vars_leg <-
   ) %>%
   pull(variable)
 
-# Political institutions ======================
+# 8. Political institutions ======================
 vars_pol <-
   db_variables %>%
   filter(
@@ -52,16 +78,25 @@ vars_pol <-
   ) %>%
   pull(variable)
 
-# Public sector performance institutions ======================
-vars_publ <-
+# 9. Public Human Resource Management Institutions ======================
+vars_hrm <-
   db_variables %>%
   filter(
     var_level == "indicator",
-    family_var == "vars_publ"
+    family_var == "vars_hrm"
   ) %>%
   pull(variable)
 
-# Social institutions ======================
+# 10. Public Public Financial Management Institutions ======================
+vars_pfm <-
+  db_variables %>%
+  filter(
+    var_level == "indicator",
+    family_var == "vars_pfm"
+  ) %>%
+  pull(variable)
+
+# 11. Social institutions ======================
 vars_social <-
   db_variables %>%
   filter(
@@ -70,12 +105,57 @@ vars_social <-
   ) %>%
   pull(variable)
 
-# SOE Corporate Governance ======================
+# 12. SOE Corporate Governance ======================
 vars_service_del <-
   db_variables %>%
   filter(
     var_level == "indicator",
     family_var == "vars_service_del"
+  ) %>%
+  pull(variable)
+
+# 13. Service Delivery Institutions ======================
+vars_service_delivery <-
+  db_variables %>%
+  filter(
+    var_level == "indicator",
+    family_var == "vars_service_delivery"
+  ) %>%
+  pull(variable)
+
+# 14. Transparency and Accountability institutions ======================
+vars_transp <-
+  db_variables %>%
+  filter(
+    var_level == "indicator",
+    family_var == "vars_transp"
+  ) %>%
+  pull(variable)
+
+# variables with other families ======================
+vars_other <-
+  db_variables %>%
+  filter(
+    var_level == "indicator",
+    family_var == "vars_other"
+  ) %>%
+  pull(variable)
+
+# removed variables ======================
+vars_removed <-
+  db_variables %>%
+  filter(
+    var_level == "indicator",
+    family_var == "vars_removed"
+  ) %>%
+  pull(variable)
+
+# variables with missing families ======================
+vars_missing <-
+  db_variables %>%
+  filter(
+    var_level == "indicator",
+    family_var == "vars_missing"
   ) %>%
   pull(variable)
 
--- app/global.R
+++ app/global.R
@@ -133,7 +133,7 @@ for(i in 1:nrow(clean_country)){
   if (clean_country[i,'Clean_Names']!=""){
     country_list$country_name[country_list$country_name==clean_country[i,'Country']]=clean_country[i,'Clean_Names']
     ctf_long$country_name[ctf_long$country_name==clean_country[i,'Country']]=clean_country[i,'Clean_Names']
-    raw_data$country_name[raw_data$country_name==clean_country[i,'Country']]=clean_country[i,'Clean_Names']
+    # raw_data$country_name[raw_data$country_name==clean_country[i,'Country']]=clean_country[i,'Clean_Names']
     global_data$country_name[global_data$country_name==clean_country[i,'Country']]=clean_country[i,'Clean_Names']
     spatial_data$country_name[spatial_data$country_name==clean_country[i,'Country']]=clean_country[i,'Clean_Names']
  }
