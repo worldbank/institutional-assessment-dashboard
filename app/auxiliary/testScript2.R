82b013153daea9ac4d4c5d5cd1b2c1c7698bded8
Shelmith Kariuki <kariukishelmith@gmail.com>
2023-09-17 00:58:18 +0100

    I use this script for testing. Please do not delete it.

---
+++ app/auxiliary/testScript2.R
@@ -0,0 +1,163 @@
+data = global_data
+Category = "Custom"
+Grp = c("GRP1", "GRP1","GRP1", "GRP2", "GRP2", "GRP2", "GRP2", "GRP2", "GRP3", "GRP3", "GRP3", "GRP3")
+Countries <- c("Denmark", "Russian Federation", "Sweden", "Tajikistan",
+  "Thailand", "Trinidad and Tobago", "Tunisia", "Turkmenistan", "Uzbekistan", "Venezuela, RB",
+  "Vietnam", "Yemen, Rep.")
+
+custom_df <- data.frame(Category, Grp, Countries)
+
+base_country <- "Angola"
+comparison_countries = country_list %>%
+  filter(group == "Sub-Saharan Africa" & country_name != base_country) %>%
+  distinct(country_name) %>%
+  pull()
+comparison_countries = NULL
+var = "Right to information"
+
+groups = c("Sub-Saharan Africa", unique(custom_df$Grp)[1:3])
+
+# data = global_data
+# base_country =  input$country_bar
+# comparison_countries = input$countries_bar
+# groups = input$groups_bar
+# var = input$vars_bar
+# variable_names = variable_names
+# custom_df = custom_df()
+
+# static_bar(
+#   global_data,
+#   base_country,
+#   comparison_countries,
+#   groups,
+#   var,
+#   variable_names,
+#   custom_df
+# )
+# 
+# country_list %>%
+#   filter(group %in% "Sub-Saharan Africa") %>%
+#   select(group, country_code)
+# 
+# x <- country_list %>%
+#   filter(country_name %in% custom_df$Countries) %>%
+#   select(group, country_code)
+
+raw_data = raw_data
+vars_trends = "Absence of corruption"
+var = 
+  db_variables %>%
+  filter(var_name == vars_trends) %>%
+  pull(variable)
+
+country_trends = base_country
+countries_trends = NULL
+country_list = country_list
+group_trends = groups
+db_variables = db_variables
+custom_df_trend = custom_df
+
+
+indicator = var
+indicator_name = vars_trends
+base_country
+comparison_countries
+country_list
+groups = groups
+definitions = db_variables
+custom_df
+
+trends_plot(
+  raw_data,
+  var,
+  vars_trends,
+  country_trends,
+  countries_trends,
+  country_list,
+  group_trends,
+  db_variables,
+  custom_df_trend
+)
+
+
+x <- custom_df %>% 
+      group_by(Grp) %>% 
+      mutate(note = paste0(Grp, " (", paste0(Countries, collapse = " , ") , ")")) %>% 
+      distinct(note) %>% 
+      pull() %>% 
+      paste0(., collapse = " ; ")
+
+
+
+
+
+x <- family_data(
+  global_data,
+  base_country(),
+  variable_names
+)
+
+data = x
+base_country = base_country()
+country_list = country_list
+comparison_countries = input$countries
+vars = vars_family
+variable_names = family_names
+threshold = input$threshold
+
+plot_notes_function(y, z, tab_name, miss_var, plot_type, custom_df) 
+
+plot_notes_function(
+  base_country(),
+  note_compare(),
+  input$family,
+  missing_variables,
+  "static",
+  custom_df = custom_df()
+  
+)
+interactive_plot(x, tab_name, buttons,  plot_type) 
+
+
+interactive_plot(
+  base_country(),
+  note_compare(),
+  input$family,
+  plotly_remove_buttons,
+  missing_variables,
+  "dynamic",
+  custom_df = custom_df()
+)
+
+data =  global_data_dyn %>%
+  def_quantiles_dyn(
+    base_country(),
+    country_list,
+    input$countries,
+    vars_all,
+    variable_names,
+    input$threshold
+  )
+
+base_country = base_country()
+tab_name = input$family
+rank = input$rank
+group_median = NULL
+custom_df = NULL ## New addition made by Shel in August 2023 to accommodate custom groups
+title = TRUE
+dots = FALSE
+note = NULL
+threshold = input$threshold
+preset_order = input$preset_order
+
+data =  global_data %>%
+  def_quantiles(
+    base_country(),
+    country_list,
+    input$countries,
+    vars_all,
+    variable_names,
+    input$threshold
+  )
+
+
