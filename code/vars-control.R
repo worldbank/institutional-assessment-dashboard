48440e5effcc77098ae3ba7816f5e318231ed4a7
Galileu Kim <galileukim@worldbank.org>
2023-09-22 15:18:05 -0400

    fix: update vars-control.R file and remove unwanted csv files from output folder.

--- code/vars-control.R
+++ code/vars-control.R
@@ -1,13 +1,13 @@
-# Anti-Corruption, Transparency and Accountability institutions ======================
-vars_transp <-
+# 1. Anti-corruption institutions ======================
+vars_anticorruption <-
   db_variables %>%
   filter(
     var_level == "indicator",
-    family_var == "vars_transp"
+    family_var == "vars_anticorruption"
   ) %>%
   pull(variable)
 
-# Business environment and trade institutions ======================
+# 2. Business environment and trade institutions ======================
 vars_mkt <-
   db_variables %>%
   filter(
@@ -16,7 +16,25 @@ vars_mkt <-
   ) %>%
   pull(variable)
 
-# Financial market institutions ======================
+# 3. Climate Change and Environment Institutions ======================
+vars_climate <-
+  db_variables %>%
+  filter(
+    var_level == "indicator",
+    family_var == "vars_climate"
+  ) %>%
+  pull(variable)
+
+# 4. Digital and Data Institutions ======================
+vars_digital <-
+  db_variables %>%
+  filter(
+    var_level == "indicator",
+    family_var == "vars_digital"
+  ) %>%
+  pull(variable)
+
+# 5. Financial market institutions ======================
 vars_fin <-
   db_variables %>%
   filter(
@@ -25,7 +43,7 @@ vars_fin <-
   ) %>%
   pull(variable)
 
-# Labor market institutions ======================
+# 6. Labor market institutions ======================
 vars_lab <-
   db_variables %>%
   filter(
@@ -34,7 +52,7 @@ vars_lab <-
   ) %>%
   pull(variable)
 
-# Legal institutions ======================
+# 7. Justice institutions ======================
 vars_leg <-
   db_variables %>%
   filter(
@@ -43,7 +61,7 @@ vars_leg <-
   ) %>%
   pull(variable)
 
-# Political institutions ======================
+# 8. Political institutions ======================
 vars_pol <-
   db_variables %>%
   filter(
@@ -52,16 +70,25 @@ vars_pol <-
   ) %>%
   pull(variable)
 
-# Public sector performance institutions ======================
-vars_publ <-
+# 9. Public Human Resource Management Institutions ======================
+vars_hrm <-
   db_variables %>%
   filter(
     var_level == "indicator",
-    family_var == "vars_publ"
+    family_var == "vars_hrm"
   ) %>%
   pull(variable)
 
-# Social institutions ======================
+# 10. Public Public Financial Management Institutions ======================
+vars_pfm <-
+  db_variables %>%
+  filter(
+    var_level == "indicator",
+    family_var == "vars_pfm"
+  ) %>%
+  pull(variable)
+
+# 11. Social institutions ======================
 vars_social <-
   db_variables %>%
   filter(
@@ -70,12 +97,57 @@ vars_social <-
   ) %>%
   pull(variable)
 
-# SOE Corporate Governance ======================
+# 12. SOE Corporate Governance ======================
 vars_service_del <-
   db_variables %>%
   filter(
     var_level == "indicator",
     family_var == "vars_service_del"
+  ) %>%
+  pull(variable)
+
+# 13. Service Delivery Institutions ======================
+vars_service_delivery <-
+  db_variables %>%
+  filter(
+    var_level == "indicator",
+    family_var == "vars_service_delivery"
+  ) %>%
+  pull(variable)
+
+# 14. Transparency and Accountability institutions ======================
+vars_transp <-
+  db_variables %>%
+  filter(
+    var_level == "indicator",
+    family_var == "vars_transp"
+  ) %>%
+  pull(variable)
+
+# variables with other families ======================
+vars_other <-
+  db_variables %>%
+  filter(
+    var_level == "indicator",
+    family_var == "vars_other"
+  ) %>%
+  pull(variable)
+
+# removed variables ======================
+vars_removed <-
+  db_variables %>%
+  filter(
+    var_level == "indicator",
+    family_var == "vars_removed"
+  ) %>%
+  pull(variable)
+
+# variables with missing families ======================
+vars_missing <-
+  db_variables %>%
+  filter(
+    var_level == "indicator",
+    family_var == "vars_missing"
   ) %>%
   pull(variable)
 
@@ -90,41 +162,20 @@ vars_all <-
 # Families variables ======================
 vars_family <-
   db_variables %>%
-  filter(family_name != "Other") %>%
+  filter(
+    family_var != "vars_other"
+  ) %>%
   pull(family_var) %>%
   unique
 
-# Variables from Original source  ======================
-vars_original <-
-  db_variables %>%
-  filter(
-    var_level == "indicator",
-    data_source == "original"
-  ) %>%
-  pull(variable)
-
-# Variables from Gov360 API ======================
-vars_api <-
-  db_variables %>%
-  filter(
-    var_level == "indicator",
-    data_source == "API"
-  ) %>%
-  pull(variable)
-
-# Variables from Additions source  ======================
-vars_additions <-
-  db_variables %>%
-  filter(
-    var_level == "indicator",
-    data_source == "additions"
-  ) %>%
-  pull(variable)
-
-
+# extract family names
 family_names <-
   db_variables %>%
-  filter(family_name != "Other") %>%
+  # exclude extraneous families
+  filter(
+    family_var != "vars_other" &
+      family_var != "vars_missing"
+  ) %>%
   transmute(
     variable = family_var,
     var_name = family_name
