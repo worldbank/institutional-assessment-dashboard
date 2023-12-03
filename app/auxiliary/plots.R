0d5943abaabd78269ecacf8c78a4185bf6279d26
Kannan <kannanv93@gmail.com>
2023-12-03 15:49:58 +0530

    Updated fix for #173

--- app/auxiliary/plots.R
+++ app/auxiliary/plots.R
@@ -49,13 +49,15 @@ static_plot <-
       
         base_country_df<-data%>%
           filter(country_name==base_country[1])
-   
-        base_country_df<-base_country_df%>%
-            arrange((dtt))
-          
+        
+        custom_levels <- c("Weak\n(bottom 25%)", "Emerging\n(25% - 50%)", "Strong\n(top 50%)")
+        
+        base_country_df$status <- factor(base_country_df$status, levels = custom_levels, ordered = TRUE)
+        
+        base_country_df <- base_country_df[order(base_country_df$status,base_country_df$dtf), ]
+        
         unique_indicators = base_country_df %>% 
-            distinct(var_name,dtt) %>% 
-            arrange((dtt)) %>% 
+            distinct(var_name) %>% 
             pull(var_name)
       
         data$var_name <-
