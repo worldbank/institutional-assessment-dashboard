c33b8c577febf85635bed95cae512bf862ae1fe8
Kannan <kannanv93@gmail.com>
2024-03-05 16:33:08 +0530

    Dynamic Benchmarking automatic after static

--- app/ui.R
+++ app/ui.R
@@ -501,15 +501,7 @@ ui <-
                   label = "Show notes",
                   status = "success",
                   value = FALSE
-                ),
-                column(width = 1),
-                shinyWidgets::materialSwitch(
-                  inputId = "show_dynamic_plot",
-                  label = "Show dynamic benchmark plot",
-                  status = "success",
-                  value = FALSE
                 )
-                
 
                 
               ),
@@ -542,7 +534,7 @@ ui <-
             
             
             conditionalPanel(
-              "input.select !== 0 && input.show_dynamic_plot === true",
+              "input.select !== 0 && output.plot!=null",
               fluidRow(
                 
                 column(
